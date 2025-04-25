31715bfa28d09e8ecf4d6676f3f2ab6ed8a09080
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-25 21:06:14 +0300

    feat : Add EFCore building block

---
+++ src/Libraries/Vogel.BuildingBlocks.EntityFramework/NexaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Vogel.BuildingBlocks.EntityFramework
+{
+    public abstract class NexaDbContext<TContext> : DbContext where TContext : DbContext
+    {
+        public NexaDbContext(DbContextOptions<TContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/Libraries/Vogel.BuildingBlocks.EntityFramework/Repositories/EFCoreRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexa.BuildingBlocks.Domain.NewFolder;
+using Nexa.BuildingBlocks.Domain;
+using System.Linq.Expressions;
+namespace Vogel.BuildingBlocks.EntityFramework.Repositories
+{
+    public abstract class EFCoreRepository<TEntity, TContext> : IRepository<TEntity>
+         where TEntity : class, IEntity
+         where TContext : DbContext
+    {
+        private readonly TContext _dbContext;
+
+        public EFCoreRepository(TContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<TEntity> AsQuerable()
+        {
+            return _dbContext.Set<TEntity>().AsQueryable();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<TEntity?> FindByIdAsync(object id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<List<TEntity>> InsertManyAsync(List<TEntity> entities)
+        {
+            await _dbContext.Set<TEntity>().AddRangeAsync(entities);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entities;
+        }
+
+        public async Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _dbContext.Set<TEntity>().SingleAsync(expression);
+        }
+
+        public async Task<TEntity?> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _dbContext.Set<TEntity>().SingleOrDefaultAsync(expression);
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            return await _dbContext.Set<TEntity>().AnyAsync(expression);
+        }
+
+    }
+}
