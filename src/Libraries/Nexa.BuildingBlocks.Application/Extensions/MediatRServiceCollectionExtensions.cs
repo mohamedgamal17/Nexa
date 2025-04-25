1995b612701eea2e407c9fe4ebf502dbd7fd3fa9
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-25 20:47:34 +0300

    feat : Add MediatR ServiceCollection Extensions

---
+++ src/Libraries/Nexa.BuildingBlocks.Application/Extensions/MediatRServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.BuildingBlocks.Application.Behaviours;
+namespace Nexa.BuildingBlocks.Application.Extensions
+{
+    public static class MediatRServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterMediatRCommonPibelineBehaviors(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IRequestPostProcessor<,>), typeof(LoggingBehaviour<>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ExceptionHandlingBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+            return services;
+        }
+    }
+}
