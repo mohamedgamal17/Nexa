e88751dc5bd21786b4e26c3709aee6de8512bd51
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-26 01:37:16 +0300

    feat(accounting) : Add AccountingRepository

---
+++ src/Modules/Accoounting/Nexa.Accounting.Domain/IAccountingRepository.cs
@@ -0,0 +1,8 @@
+﻿using Nexa.BuildingBlocks.Domain.NewFolder;
+
+namespace Nexa.Accounting.Domain
+{
+    public interface IAccountingRepository<TEntity> : IRepository<TEntity> where TEntity  :class
+    {
+    }
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/EntityFramework/AccountingRepository.cs
@@ -0,0 +1,15 @@
+﻿using Nexa.Accounting.Domain;
+using Nexa.BuildingBlocks.Domain;
+using Vogel.BuildingBlocks.EntityFramework.Repositories;
+
+namespace Nexa.Accounting.Infrastructure.EntityFramework
+{
+    public class AccountingRepository<TEntity> : EFCoreRepository<TEntity, AccountingDbContext>, IAccountingRepository<TEntity>
+        where TEntity : class ,IEntity
+    {
+        public AccountingRepository(AccountingDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
