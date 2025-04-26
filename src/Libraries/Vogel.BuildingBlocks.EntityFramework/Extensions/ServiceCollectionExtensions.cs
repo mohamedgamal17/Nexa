810fc57f40e7d3ca421ffac392dad26442f45ec4
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-26 14:16:28 +0300

    feat : Add EFCore domain even dispatcher interceptor

---
+++ src/Libraries/Vogel.BuildingBlocks.EntityFramework/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Vogel.BuildingBlocks.EntityFramework.Interceptors;
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterEfCoreInterceptors(this IServiceCollection services)
+        {
+            services.AddScoped<DispatchDomainEventInterceptor>();
+
+            services.AddTransient<TimeProvider>(_ => TimeProvider.System);
+
+            return services;
+        }
+    }
+}
---
+++ src/Libraries/Vogel.BuildingBlocks.EntityFramework/Interceptors/DispatchDomainEventInterceptor.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Nexa.BuildingBlocks.Domain;
+namespace Vogel.BuildingBlocks.EntityFramework.Interceptors
+{
+    public class DispatchDomainEventInterceptor : SaveChangesInterceptor
+    {
+        private readonly IMediator _mediator;
+
+        public DispatchDomainEventInterceptor(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            await DispatchDomainEvents(eventData.Context);
+
+            return await base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+
+        private async Task DispatchDomainEvents(DbContext? dbContext)
+        {
+            if (dbContext == null)
+            {
+                return;
+            }
+
+            var entries = dbContext.ChangeTracker.Entries();
+
+            var aggregateRootEntries = dbContext.ChangeTracker.Entries<IAggregateRoot>();
+
+            foreach (var entry in aggregateRootEntries)
+            {
+                if (entry.Entity.Events.Count > 0)
+                {
+                    foreach (var @event in entry.Entity.Events)
+                    {
+                        await _mediator.Publish(@event);
+
+                        entry.Entity.ClearDomainEvents();
+
+                    }
+                }
+            }
+        }
+    }
+}
