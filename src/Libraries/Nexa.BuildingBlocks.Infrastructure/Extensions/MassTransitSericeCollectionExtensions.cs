47a40ce76c23ef2bd1d8e0340a176dae8e23ee91
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-27 16:12:46 +0300

    feat : Add MassTransitServicesCollectionExtensions

---
+++ src/Libraries/Nexa.BuildingBlocks.Infrastructure/Extensions/MassTransitSericeCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using MassTransit.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+namespace Nexa.BuildingBlocks.Infrastructure.Extensions
+{
+    public static class MassTransitSericeCollectionExtensions
+    {
+        public static IServiceCollection RegisterMassTransitConsumers(this IServiceCollection services, Assembly assembly)
+        {
+            var consumerTypes = assembly.GetTypes()
+                .Where(x => x.IsClass)
+                .Where(x => !x.IsAbstract)
+                .Where(c => typeof(IConsumer<>).IsAssignableFrom(c))
+                .ToList();
+
+
+            var registerConsumer = typeof(DependencyInjectionConsumerRegistrationExtensions)
+                .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Single(m =>
+                    m.Name == "RegisterConsumer" &&
+                    m.GetGenericArguments().Length == 1 &&
+                    m.GetParameters().Length == 1);
+
+
+            foreach (var consumerType in consumerTypes)
+            {
+                registerConsumer.MakeGenericMethod(consumerType).Invoke(services, [services]);
+            }
+
+            return services;
+        }
+
+    }
+}
