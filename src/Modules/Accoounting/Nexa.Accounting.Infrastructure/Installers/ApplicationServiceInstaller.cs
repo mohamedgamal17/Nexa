954fd560da8b044e78d968c2e938e05c1d9d9933
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-28 01:46:42 +0300

    feat(accounting) : Add initial module services installers

---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/Installers/ApplicationServiceInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexa.Accounting.Application.Transactions.Services;
+using Nexa.Accounting.Application.Wallets.Services;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+
+namespace Nexa.Accounting.Infrastructure.Installers
+{
+    public class ApplicationServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly))
+                .AddAutoMapper(Cfg => Cfg.AddMaps(Application.AssemblyReference.Assembly))
+                .RegisterPoliciesHandlerFromAssembly(Application.AssemblyReference.Assembly)
+                .AddTransient<IWalletNumberGeneratorService, WalletNumberGeneratorService>()
+                .AddTransient<ITransactionNumberGeneratorService, TransactionNumberGeneratorService>();
+        }
+    }
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/Installers/EntityFrameworkServiceInstaller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexa.Accounting.Domain;
+using Nexa.Accounting.Infrastructure.EntityFramework;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using Vogel.BuildingBlocks.EntityFramework.Interceptors;
+using Microsoft.EntityFrameworkCore;
+namespace Nexa.Accounting.Infrastructure.Installers
+{
+    internal class EntityFrameworkServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.AddDbContext<AccountingDbContext>((sp, opt) =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("Default"), cfg => cfg
+                    .UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+                   )
+                .AddInterceptors(
+                        sp.GetRequiredService<DispatchDomainEventInterceptor>()
+                   );
+            });
+
+            services.AddTransient(typeof(IAccountingRepository<>), typeof(AccountingRepository<>));
+        }
+    }
+}
