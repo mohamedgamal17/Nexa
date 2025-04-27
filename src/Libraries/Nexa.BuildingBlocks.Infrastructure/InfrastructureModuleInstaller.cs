a42ff576fbabc7cb1fad417b4740924590fd92dd
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-27 16:13:10 +0300

    feat : Add base infrastructure module installer

---
+++ src/Libraries/Nexa.BuildingBlocks.Infrastructure/InfrastructureModuleInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using Nexa.BuildingBlocks.Infrastructure.Security;
+namespace Nexa.BuildingBlocks.Infrastructure
+{
+    internal class InfrastructureModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            ConfigureSecurity(services);
+        }
+
+
+        private static void ConfigureSecurity(IServiceCollection services)
+        {
+
+            services.AddTransient<IApplicationAuthorizationService, ApplicationAuthorizationService>();
+
+            services.AddTransient<ISecurityContext, SecurityContext>();
+        }
+    }
+}
