eed27b41c7be95aa20499437623bd7efd7237d35
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-04-28 01:49:53 +0300

    feat(accounting) : Add accounting module installer

---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/AccountingModuleBootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.Accounting.Infrastructure.EntityFramework;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+namespace Nexa.Accounting.Infrastructure
+{
+    internal class AccountingModuleBootStrapper : IModuleBootstrapper
+    {
+        public async Task Bootstrap(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<AccountingDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/AccountingModuleInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using System.Reflection;
+namespace Nexa.Accounting.Infrastructure
+{
+    public class AccountingModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+        {
+            services.InstallServiceFromAssembly(configuration, environment,
+             Assembly.GetExecutingAssembly()
+           );
+
+            services.AddTransient<IModuleBootstrapper, AccountingModuleBootStrapper>();
+        }
+    }
+}
