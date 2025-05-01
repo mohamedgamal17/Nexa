bb7e644b4407beb55d341728be4e82a76f5b7afd
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-01 22:53:38 +0300

    fix : Fix RegisterFactoriesFromAssembly  function

--- src/Libraries/Nexa.BuildingBlocks.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/Libraries/Nexa.BuildingBlocks.Application/Extensions/ServiceCollectionExtensions.cs
@@ -10,12 +10,12 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             var types = assembly.GetTypes()
             .Where(x => x.IsClass)
-            .Where(x => x.GetInterfaces().Any(c => c.GetGenericTypeDefinition() == typeof(IResponseFactory<,>)))
+            .Where(x => x.GetInterfaces().Any(c =>c.IsGenericType && c.GetGenericTypeDefinition() == typeof(IResponseFactory<,>)))
             .ToList();
 
             foreach (var type in types)
             {
-                services.AddTransient(type.GetInterfaces().First(), type);
+                services.AddTransient(type.GetInterfaces().Where(x=> !x.IsGenericType).First(), type);
             }
 
             return services;
