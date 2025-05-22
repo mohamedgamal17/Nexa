ce0106b980672195b1ad4c663e81ba6d6cd22246
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-22 16:37:59 +0300

    fix : update sender wallet balance after operation completed

--- src/Modules/Accoounting/Nexa.Accounting.Application/Transactions/Consumers/ProcessInternalTransactionIntgertationEventConsumer.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Transactions/Consumers/ProcessInternalTransactionIntgertationEventConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
 using Nexa.Accounting.Application.Transactions.Events;
 using Nexa.Accounting.Domain;
 using Nexa.Accounting.Domain.Transactions;
@@ -10,18 +11,23 @@ namespace Nexa.Accounting.Application.Transactions.Consumers
     {
         private readonly IAccountingRepository<InternalTransaction> _transactionRepository;
         private readonly IAccountingRepository<Wallet> _walletRepository;
-
-        public ProcessInternalTransactionIntgertationEventConsumer(IAccountingRepository<InternalTransaction> transactionRepository, IAccountingRepository<Wallet> walletRepository)
+        private readonly ILogger<ProcessInternalTransactionIntgertationEventConsumer> logger;
+        public ProcessInternalTransactionIntgertationEventConsumer(IAccountingRepository<InternalTransaction> transactionRepository, IAccountingRepository<Wallet> walletRepository, ILogger<ProcessInternalTransactionIntgertationEventConsumer> logger)
         {
             _transactionRepository = transactionRepository;
             _walletRepository = walletRepository;
+            this.logger = logger;
         }
 
         public async Task Consume(ConsumeContext<ProcessInternalTransactionIntgertationEvent> context)
         {
             var transaction = await _transactionRepository.SingleAsync(x => x.Id == context.Message.TransactionId);
 
-            var reciverWallet = await _walletRepository.SingleAsync(x => x.Id == transaction.WalletId);
+            var senderWallet = await _walletRepository.SingleAsync(x => x.Id == transaction.WalletId);
+
+            var reciverWallet = await _walletRepository.SingleAsync(x => x.Id == transaction.ReciverId);
+
+            senderWallet.Depit(transaction.Amount);
 
             reciverWallet.Credit(transaction.Amount);
 
@@ -29,7 +35,11 @@ namespace Nexa.Accounting.Application.Transactions.Consumers
 
             await _transactionRepository.UpdateAsync(transaction);
 
+            await _walletRepository.UpdateAsync(senderWallet);
+
             await _walletRepository.UpdateAsync(reciverWallet);
+
+            logger.LogInformation("Consumer Completed");
 
         }
     }
