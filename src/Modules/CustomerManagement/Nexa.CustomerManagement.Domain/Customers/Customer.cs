883701a8696ce26266f45f0a552a54f6ae50cea7
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-26 15:10:10 +0300

    feat(customer-management) : Add customer enitity UserId property

--- src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
@@ -4,6 +4,7 @@ namespace Nexa.CustomerManagement.Domain.Customers
 {
     public class Customer : BaseEntity
     {
+        public string UserId { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
@@ -13,9 +14,9 @@ namespace Nexa.CustomerManagement.Domain.Customers
         public DateTime BirthDate { get; set; }
         public Gender Gender { get; set; }
         public Address Address { get; set; }
-        public string SocialSecurityNumber { get; set; }
-        public string SocialInsuranceNumber { get; set; }
-        public string NationalIdentityNumber { get; set; }
-        public string TaxIdentificationNumber { get; set; }
+        public string? SocialSecurityNumber { get; set; }
+        public string? SocialInsuranceNumber { get; set; }
+        public string? NationalIdentityNumber { get; set; }
+        public string? TaxIdentificationNumber { get; set; }
     }
 }
