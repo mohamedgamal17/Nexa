393fdbde6b391b9771e433cc8fe8b14dfce6d971
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-26 19:20:29 +0300

    feat(customer-management) : Add initial database migrations

---
+++ src/Modules/CustomerManagement/Nex.CustomerManagement.Infrastructure/EntityFramework/Migrations/20250526161714_InitialMigration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexa.CustomerManagement.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "CustomerManagement");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                schema: "CustomerManagement",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    SocialSecurityNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    SocialInsuranceNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    NationalIdentityNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    TaxIdentificationNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                schema: "CustomerManagement",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    StreetLine1 = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    StreetLine2 = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Customers_Id",
+                        column: x => x.Id,
+                        principalSchema: "CustomerManagement",
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_UserId",
+                schema: "CustomerManagement",
+                table: "Customers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses",
+                schema: "CustomerManagement");
+
+            migrationBuilder.DropTable(
+                name: "Customers",
+                schema: "CustomerManagement");
+        }
+    }
+}
