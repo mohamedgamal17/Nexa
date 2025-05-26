34ddc5f833988c5045bbbb3a7a316770408223a4
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-26 15:48:54 +0300

    feat(customer-management) : Add efcore table constants

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/CustomerTableConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace Nexa.CustomerManagement.Domain.Customers
+{
+    public static class CustomerTableConstants
+    {
+        public const int IdLength = 256;
+
+        public const int FirstNameLength = 256;
+
+        public const int MiddleNameLength = 256;
+
+        public const int LastNameLength = 256;
+
+        public const int NationalityLength = 3;
+
+        public const int PhoneNumberLength = 20;
+
+        public const int EmailAddressLength = 256;
+
+        public const int SocialNumberSecurityLength = 100;
+
+        public const int SocialInsuranceNumberLength = 100;
+
+        public const int NationalIdentityNumberLength = 100;
+
+        public const int TaxIdentificationNumberLength = 100;
+    }
+}
