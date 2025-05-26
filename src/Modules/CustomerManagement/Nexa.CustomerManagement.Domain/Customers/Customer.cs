4c6833baf579370c47a5bb42e3a4b6af36636ccb
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-05-26 13:51:28 +0300

    feat(customer-management) : Add additional Customer entity properties for kyc verification

--- src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
@@ -7,9 +7,15 @@ namespace Nexa.CustomerManagement.Domain.Customers
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-        public string Citizenship { get; set; }
+        public string Nationality { get; set; }
         public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
         public Address Address { get; set; }
-        public string SSN { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string SocialInsuranceNumber { get; set; }
+        public string NationalIdentityNumber { get; set; }
+        public string TaxIdentificationNumber { get; set; }
     }
 }
