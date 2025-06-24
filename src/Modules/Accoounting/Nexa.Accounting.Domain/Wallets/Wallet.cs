5ee01cb76fd34ea58193c107c6c19f1c9184bad6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-24 17:18:12 +0300

    feat(acconting) : Add ReservedBalance property in wallet domain entity

--- src/Modules/Accoounting/Nexa.Accounting.Domain/Wallets/Wallet.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Domain/Wallets/Wallet.cs
@@ -8,6 +8,7 @@ namespace Nexa.Accounting.Domain.Wallets
         public string Number { get; private set; }
         public string UserId { get; private set; }
         public decimal Balance { get; private set; }
+        public decimal ReservedBalance { get; private set; }
 
         //Constructor for efcore
         private Wallet() { }
@@ -30,10 +31,12 @@ namespace Nexa.Accounting.Domain.Wallets
         {
             Guard.Against.NegativeOrZero(amount);
 
-            if(Balance < amount)
+            if (ReservedBalance < amount)
             {
-                throw new InvalidOperationException("Insufficient balance to complete the depit operation.");
+                throw new InvalidOperationException($"Insufficient reserved balance : ({ReservedBalance}) , cannot relase amount : ({amount}) .");
             }
+
+            ReservedBalance -= amount;
 
             Balance -= amount;
         }
@@ -42,6 +45,35 @@ namespace Nexa.Accounting.Domain.Wallets
         public void Credit(decimal amount)
         {
             Guard.Against.NegativeOrZero(amount);
+
+            Balance += amount;
+        }
+
+
+        public void Reserve(decimal amount)
+        {
+            Guard.Against.NegativeOrZero(amount);
+
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Insufficient balance to reserved");
+            }
+
+            Balance -= amount;
+
+            ReservedBalance += amount; 
+        }
+
+        public void Release(decimal amount)
+        {
+            Guard.Against.NegativeOrZero(amount);
+
+            if (ReservedBalance < amount)
+            {
+                throw new InvalidOperationException($"Insufficient reserved balance : ({ReservedBalance}) , cannot relase amount : ({amount}) .");
+            }
+
+            ReservedBalance -= amount;
 
             Balance += amount;
         }
---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/EntityFramework/Migrations/20250623160503_ResservedBalanceMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexa.Accounting.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResservedBalanceMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ReservedBalance",
+                schema: "Accounting",
+                table: "Wallet",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReservedBalance",
+                schema: "Accounting",
+                table: "Wallet");
+        }
+    }
+}
