5c5f9f54bfdd9c0ed25b77df997a068ceee8606e
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 23:32:02 +0300

    test(transaction) : Add initial test cases for VerifiyTransferIntegrationEventConsumer

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/VerifiyTransferIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,54 @@
+﻿using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Shared.Enums;
+using Nexa.Transactions.Shared.Events;
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_verifiy_transfer_integration_event_consumed : TransferTestFixture
+    {
+        [Test]
+        public async Task Should_publish_reserve_wallet_balance_message_when_current_transfer_is_network_transfer()
+        {
+            var transfer = await CreateRandomNetworkTransfer();
+
+            var message = new VerifiyTransferIntegrationEvent(transfer.Id, transfer.Number, transfer.WalletId, transfer.Amount, TransferType.Network);
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<VerifiyTransferIntegrationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<ReserveWalletBalanceIntegrationEvent>());
+        }
+
+        [Test]
+        public async Task Should_publish_reserve_wallet_balance_message_when_current_transfer_is_bank_transfer_and_direction_is_depit()
+        {
+            var transfer = await CreateRandomBankTransferAsync(BankTransferType.Ach,TransferDirection.Depit);
+
+            var message = new VerifiyTransferIntegrationEvent(transfer.Id, transfer.Number, transfer.WalletId, transfer.Amount, transfer.Type);
+
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<VerifiyTransferIntegrationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<ReserveWalletBalanceIntegrationEvent>());
+        }
+
+        [Test]
+        public async Task Should_publish_transfer_verified_message_when_current_transfer_is_bank_transfer_and_direction_is_credit()
+        {
+            var transfer = await CreateRandomBankTransferAsync(BankTransferType.Ach, TransferDirection.Credit);
+
+            var message = new VerifiyTransferIntegrationEvent(transfer.Id, transfer.Number, transfer.WalletId, transfer.Amount, transfer.Type);
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<VerifiyTransferIntegrationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<TransferVerifiedIntegrationEvent>());
+        }
+
+
+    }
+}
--- tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/TransferTestFixture.cs
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/TransferTestFixture.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using Nexa.Accounting.Shared.Dtos;
 using Nexa.Transactions.Application.Tests.Fakers;
+using Nexa.Transactions.Domain;
 using Nexa.Transactions.Domain.Transfers;
+using Nexa.Transactions.Shared.Enums;
 namespace Nexa.Transactions.Application.Tests.Transfers
 {
     public class TransferTestFixture : TransactionsTestFixture
@@ -14,6 +17,20 @@ namespace Nexa.Transactions.Application.Tests.Transfers
             WalletService = ServiceProvider.GetRequiredService<FakeWalletService>();
             TransferRepository = ServiceProvider.GetRequiredService<ITransferRepository>();
         }
+
+        protected override async Task InitializeAsync(IServiceProvider services)
+        {
+            await base.InitializeAsync(services);
+
+            await TestHarness.Start();
+        }
+
+        protected override async Task ShutdownAsync(IServiceProvider services)
+        {
+            await base.ShutdownAsync(services);
+
+            await TestHarness.Stop();
+        }
         public async Task<WalletDto> CreateWalletAsync(string? userId = null, decimal balance = 0)
         {
             var wallet = new WalletDto
@@ -25,6 +42,43 @@ namespace Nexa.Transactions.Application.Tests.Transfers
             };
 
             return await WalletService.AddWalletAsync(wallet);
+        }
+
+        public async Task<NetworkTransfer> CreateRandomNetworkTransfer(TransferStatus status = TransferStatus.Pending)
+        {
+            var senderWallet = await CreateWalletAsync(balance: 1000);
+            var reciverWallet = await CreateWalletAsync();
+            return await CreateNetworkTransferAsync(senderWallet.Id, reciverWallet.Id, 500 , status);
+        }
+
+        public async Task<NetworkTransfer> CreateNetworkTransferAsync(string walletId , string reciverId , decimal amount, TransferStatus status)
+        {
+            return await WithScopeAsync(async (sp) =>
+            {
+                var transferRepository = sp.GetRequiredService<ITransactionRepository<NetworkTransfer>>();
+                var networkTransfer = new NetworkTransfer(walletId, reciverId, Guid.NewGuid().ToString(), amount,status);
+
+                return await transferRepository.InsertAsync(networkTransfer);
+            });
+        }
+
+        public async Task<BankTransfer> CreateRandomBankTransferAsync(BankTransferType bankTransferType , TransferDirection direction , TransferStatus status = TransferStatus.Pending)
+        {
+            var wallet = await CreateWalletAsync(balance: 1000);
+
+            return await CreateBankTransferAsync(wallet.Id, bankTransferType, direction, 500, status);
+        }
+         
+        public async Task<BankTransfer> CreateBankTransferAsync(string walletId ,BankTransferType bankTransferType  , TransferDirection direction,decimal amount , TransferStatus status )
+        {
+            return await WithScopeAsync(async (sp) =>
+            {
+                var transferRepository = sp.GetRequiredService<ITransactionRepository<BankTransfer>>();
+
+                var bankTransfer = new BankTransfer(walletId, Guid.NewGuid().ToString(), amount, Guid.NewGuid().ToString(), direction, bankTransferType, status);
+
+                return await transferRepository.InsertAsync(bankTransfer);
+            });
         }
     }
 }
