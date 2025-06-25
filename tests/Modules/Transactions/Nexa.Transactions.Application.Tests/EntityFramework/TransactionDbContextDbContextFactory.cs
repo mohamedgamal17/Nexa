8deaa4b1012c0c076c00774ed776d80ff587d131
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 18:09:18 +0300

    test(transactions) : Add initial Nexa.Transactions.Application.Tests

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/EntityFramework/TransactionDbContextDbContextFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.Transactions.Infrastructure.EntityFramework;
+namespace Nexa.Transactions.Application.Tests.EntityFramework
+{
+    public class TransactionDbContextDbContextFactory : IDesignTimeDbContextFactory<TransactionDbContext>
+    {
+        public TransactionDbContext CreateDbContext(string[] args)
+        {
+            var config = BuildConfiguration();
+
+            var builder = new DbContextOptionsBuilder<TransactionDbContext>()
+                .UseSqlServer(config.GetConnectionString("Default")!, (opt) =>
+                {
+                    opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    opt.MigrationsAssembly(typeof(TransactionDbContext).Assembly.FullName);
+                });
+
+            var services = new ServiceCollection();
+
+            var servicesProvider = services.BuildServiceProvider();
+
+            return new TransactionDbContext(builder.Options, new Mediator(servicesProvider));
+        }
+
+        private IConfigurationRoot BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Nexa.Transactions.Application.Tests/"))
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
+
