55149a859a54cc45d8420543755fc8c1f7a370e0
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 21:37:40 +0300

    fix(transactions) : Add missing authorization attribute on network transfer command

--- src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Commands/CreateNetworkTransfer/CreateNetworkTransferCommand.cs
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Commands/CreateNetworkTransfer/CreateNetworkTransferCommand.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Nexa.BuildingBlocks.Application.Requests;
 using Nexa.Transactions.Application.Transfers.Dtos;
 
 namespace Nexa.Transactions.Application.Transfers.Commands.CreateNetworkTransfer
 {
+    [Authorize]
     public class CreateNetworkTransferCommand : ICommand<TransferDto>
     {
         public string SenderId { get; set; }
