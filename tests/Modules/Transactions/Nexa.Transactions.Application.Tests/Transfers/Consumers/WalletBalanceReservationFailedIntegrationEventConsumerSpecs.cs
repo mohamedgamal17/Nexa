8961983ce816e589dc659490a4de42624cf64393
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 02:42:06 +0300

    test(transactions) : Add WalletBalanceReservationFailedIntegrationEvent initial test cases

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/WalletBalanceReservationFailedIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Shared.Enums;
+
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_wallet_balance_reservation_failed_event_consumed : TransferTestFixture
+    {
+        [Test]
+        public async Task Should_cancel_transfer()
+        {
+            var networkTransfer = await CreateRandomNetworkTransfer();
+
+            var message = new WalletBalanceReservationFailedIntegrationEvent()
+            {
+                TransferId = networkTransfer.Id,
+                WalletId = networkTransfer.WalletId,
+                Amount = networkTransfer.Amount
+            };
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<WalletBalanceReservationFailedIntegrationEvent>());
+
+            var transfer = await TransferRepository.SingleAsync(x => x.Id == networkTransfer.Id);
+
+            transfer.Status.Should().Be(TransferStatus.Faild);
+        }
+    }
+}
