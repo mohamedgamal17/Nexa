5037c4c638f92d48632e26bd8de2e227c2c66645
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 18:14:19 +0300

    test(transactions) : Add transactions base test fixture

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/TransactionsTestFixture.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.Application.Tests;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Respawn.Graph;
+using MassTransit.Testing;
+namespace Nexa.Transactions.Application.Tests
+{
+    public class TransactionsTestFixture : TestFixture
+    {
+        protected ITestHarness TestHarness { get; private set; } = null!;
+        protected override Task SetupAsync(IServiceCollection services, IConfiguration configuration)
+        {
+            services.InstallModule<TransactionsTestModuleInstaller>(configuration);
+            return Task.CompletedTask;
+        }
+        protected override async Task InitializeAsync(IServiceProvider services)
+        {
+            TestHarness = services.GetTestHarness();
+            await ResetSqlDb(services);
+            await services.RunModulesBootstrapperAsync();
+        }
+
+        protected override async Task ShutdownAsync(IServiceProvider services)
+        {
+            await ResetSqlDb(services);
+        }
+
+        protected async Task ResetSqlDb(IServiceProvider services)
+        {
+            var config = services.GetRequiredService<IConfiguration>();
+
+            var respwan = await Respawn.Respawner.CreateAsync(config.GetConnectionString("Default")!, new Respawn.RespawnerOptions
+            {
+                TablesToIgnore = new Table[]
+                {
+                  "sysdiagrams",
+                  "tblUser",
+                  "tblObjectType",
+                  "__EFMigrationsHistory"
+                },
+                SchemasToInclude = new string[]
+                {
+                    "CustomerManagement"
+                }
+
+            });
+
+            await respwan.ResetAsync(config.GetConnectionString("Default")!);
+        }
+    }
+}
---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/TransactionsTestModuleInstaller.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.Application.Tests;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using Nexa.Transactions.Infrastructure;
+namespace Nexa.Transactions.Application.Tests
+{
+    public class TransactionsTestModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.InstallModule<TransactionsModuleInstaller>(configuration)
+                .InstallModule<ApplicationTestModuleInstaller>(configuration);
+
+            services.AddMassTransitTestHarness(busRegisterConfig =>
+            {
+
+                busRegisterConfig.AddConsumers(AssemblyReference.Assembly);
+
+                busRegisterConfig.UsingInMemory((context, inMemoryBusConfig) =>
+                {
+                    inMemoryBusConfig.ConfigureEndpoints(context);
+                });
+            });
+
+        }
+    }
+}
+
