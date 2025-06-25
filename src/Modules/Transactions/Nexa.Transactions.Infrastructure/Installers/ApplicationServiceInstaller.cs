fa147b72c6ed4d170df537c2b85b9fefa250870b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 21:36:03 +0300

    fix(transactions) : Fix transaction application dependency injection registration

--- src/Modules/Transactions/Nexa.Transactions.Infrastructure/Installers/ApplicationServiceInstaller.cs
+++ src/Modules/Transactions/Nexa.Transactions.Infrastructure/Installers/ApplicationServiceInstaller.cs
@@ -9,9 +9,9 @@ namespace Nexa.Transactions.Infrastructure.Installers
         public void Install(IServiceCollection services, IConfiguration configuration)
         {
             services
-                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly))
-                .RegisterPoliciesHandlerFromAssembly(AssemblyReference.Assembly)
-                .RegisterFactoriesFromAssembly(AssemblyReference.Assembly)
+                .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly))
+                .RegisterPoliciesHandlerFromAssembly(Application.AssemblyReference.Assembly)
+                .RegisterFactoriesFromAssembly(Application.AssemblyReference.Assembly)
                 .AddTransient<ITransferNumberGeneratorService, TransferNumberGeneratorService>();
         }
     }
