a757b61a418be14f69d1580e4215f17fd6db9bb5
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 01:00:48 +0300

    test(transactions) : Add WalletBalanceReservedIntegrationEventConsumer initial test case

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/WalletBalanceReservedIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,28 @@
+﻿using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Shared.Events;
+
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_wallet_balance_reserved_integration_event_consumed : TransferTestFixture
+    {
+        [Test]
+        public async Task Should_publish_transaction_verified_integration_event()
+        {
+            var transfer = await CreateRandomNetworkTransfer();
+
+            var message = new WalletBalanceReservedIntegrationEvent()
+            {
+                TransferId = transfer.Id,
+                WalletId = transfer.WalletId,
+                Amount = transfer.Amount
+            };
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<WalletBalanceReservedIntegrationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<TransferVerifiedIntegrationEvent>());
+        }
+    }
+}
