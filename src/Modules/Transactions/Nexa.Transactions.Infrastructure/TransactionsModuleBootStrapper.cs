cd63137959a160f4a87501eb09f0981890135529
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 18:11:51 +0300

    feat(transactions) : Add base module installer and bootstrapper

---
+++ src/Modules/Transactions/Nexa.Transactions.Infrastructure/TransactionsModuleBootStrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using Nexa.Transactions.Infrastructure.EntityFramework;
+namespace Nexa.Transactions.Infrastructure
+{
+    internal class TransactionsModuleBootStrapper : IModuleBootstrapper
+    {
+        public async Task Bootstrap(IServiceProvider serviceProvider)
+        {
+            var dbContext = serviceProvider.GetRequiredService<TransactionDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
---
+++ src/Modules/Transactions/Nexa.Transactions.Infrastructure/TransactionsModuleInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.BuildingBlocks.Infrastructure.Modularity;
+using System.Reflection;
+namespace Nexa.Transactions.Infrastructure
+{
+    public class TransactionsModuleInstaller : IModuleInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.InstallServiceFromAssembly(configuration,
+             Assembly.GetExecutingAssembly()
+           );
+
+            services.AddTransient<IModuleBootstrapper, TransactionsModuleBootStrapper>();
+        }
+    }
+}
