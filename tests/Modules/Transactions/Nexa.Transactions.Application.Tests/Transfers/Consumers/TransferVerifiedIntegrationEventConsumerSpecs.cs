7803a12bef7cf046b98a00972b1f3c839e550cd2
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-25 23:43:56 +0300

    test(transactions) : Add TransferVerifiedIntegrationEventConsumer initial test cases

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/TransferVerifiedIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Nexa.Transactions.Shared.Enums;
+using Nexa.Transactions.Shared.Events;
+
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_transfer_verified_integration_event_consumed : TransferTestFixture
+    {
+        [Test]
+        public async Task Should_update_transfer_state_from_pending_to_processing()
+        {
+            var networkTransfer = await CreateRandomNetworkTransfer();
+
+            var message = new TransferVerifiedIntegrationEvent(networkTransfer.Id, networkTransfer.Number, networkTransfer.WalletId, networkTransfer.Amount, networkTransfer.Type);
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<TransferVerifiedIntegrationEvent>());
+
+            var transfer = await TransferRepository.SingleAsync(x => x.Id == networkTransfer.Id);
+
+            transfer.Status.Should().Be(TransferStatus.Processing);
+
+        }
+    }
+}
