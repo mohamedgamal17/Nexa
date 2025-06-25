c14be0bc07c4c601c11306399d83b56c9af1926d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 00:46:51 +0300

    test(transactions) : Add network funds transfer integeration event consumer initial tests

---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/NetworkFundsTransferredIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Shared.Enums;
+
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_network_funds_transferred_integration_event_consumed : TransferTestFixture
+    {
+        [Test]
+        public async Task Should_update_transfer_status_to_complete()
+        {
+            var networkTransfer = await CreateRandomNetworkTransfer(TransferStatus.Processing);
+
+            var message = new NetworkFundsTransferredIntegrationEvent(networkTransfer.Id, networkTransfer.WalletId, networkTransfer.ReciverId, networkTransfer.Amount);
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<NetworkFundsTransferredIntegrationEvent>());
+
+            var transfer = await TransferRepository.SingleAsync(x => x.Id == networkTransfer.Id);
+
+            transfer.Status.Should().Be(TransferStatus.Completed);
+        }
+    }
+}
---
+++ tests/Modules/Transactions/Nexa.Transactions.Application.Tests/Transfers/Consumers/ProcessNetworkTransferIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,25 @@
+﻿using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Shared.Enums;
+using Nexa.Transactions.Shared.Events;
+
+namespace Nexa.Transactions.Application.Tests.Transfers.Consumers
+{
+    [TestFixture]
+    public class When_process_network_transfer_integration_event_consumed : TransferTestFixture
+    {
+
+        [Test]
+        public async Task Should_publish_transfer_network__funds_integraiton_event()
+        {
+            var networkTransfer = await CreateRandomNetworkTransfer(TransferStatus.Processing);
+
+            var message = new ProcessNetworkTransferIntegrationEvent(networkTransfer.Id, networkTransfer.Number, networkTransfer.WalletId, networkTransfer.ReciverId, networkTransfer.Amount);
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<ProcessNetworkTransferIntegrationEvent>());
+
+            Assert.That(await TestHarness.Published.Any<TransferNetworkFundsIntegrationEvent>());
+        }
+    }
+}
