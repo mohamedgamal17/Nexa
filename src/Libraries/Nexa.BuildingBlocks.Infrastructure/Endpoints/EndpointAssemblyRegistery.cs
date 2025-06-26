11abe09d5e5271a3003a8f1b4f74e3e7d711e901
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 18:13:27 +0300

    feat : Add FastEndpoint assembly registeration

---
+++ src/Libraries/Nexa.BuildingBlocks.Infrastructure/Endpoints/EndpointAssemblyRegistery.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+namespace Nexa.BuildingBlocks.Infrastructure.Endpoints
+{
+    public class EndpointAssemblyRegistery
+    {
+        public HashSet<Assembly> Assemblies { get; set; }
+
+        public EndpointAssemblyRegistery()
+        {
+            Assemblies = new();
+        }
+
+        public EndpointAssemblyRegistery RegisterAssembelies(params Assembly[] asssemblies)
+        {
+            if (asssemblies != null && asssemblies.Count() > 0)
+            {
+                foreach (var assmbely in asssemblies)
+                {
+                    Assemblies.Add(assmbely);
+                }
+            }
+
+            return this;
+        }
+    }
+}
---
+++ src/Libraries/Nexa.BuildingBlocks.Infrastructure/Endpoints/EndpointAssemblyRegisteryServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+namespace Nexa.BuildingBlocks.Infrastructure.Endpoints
+{
+    public static class EndpointAssemblyRegisteryServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterEndpoints(this IServiceCollection services, Assembly assembly)
+        {
+            var registery = services.GetSinglatonOrNull<EndpointAssemblyRegistery>();
+
+            if (registery == null)
+            {
+                registery = new EndpointAssemblyRegistery();
+
+                services.AddSingleton(registery);
+            }
+
+            registery.RegisterAssembelies(assembly);
+
+            return services;
+        }
+    }
+}
