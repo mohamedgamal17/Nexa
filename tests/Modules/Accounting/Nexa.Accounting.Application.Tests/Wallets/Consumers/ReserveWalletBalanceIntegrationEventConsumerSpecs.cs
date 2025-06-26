b2b740cff4308d2f16ed2f429abc402944a6e560
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 03:47:14 +0300

    test(accounting) : Add ReserveWalletBalanceIntegrationEventConsumer initial test caeses

---
+++ tests/Modules/Accounting/Nexa.Accounting.Application.Tests/Wallets/Consumers/ReserveWalletBalanceIntegrationEventConsumerSpecs.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Nexa.Accounting.Shared.Events;
+
+namespace Nexa.Accounting.Application.Tests.Wallets.Consumers
+{
+    [TestFixture]
+    public class When_reserve_wallet_balance_event_consumed : WalletTestFixture
+    {
+        [Test]
+        public async Task Should_reserve_wallet_balance_and_publish_wallet_balance_reserved_event()
+        {
+            var fakeWallet = await CreateWalletAsync(balance : 1000);
+
+            var message = new ReserveWalletBalanceIntegrationEvent()
+            {
+                TransferId = Guid.NewGuid().ToString(),
+                WalletId = fakeWallet.Id,
+                Amount = 200
+            };
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<ReserveWalletBalanceIntegrationEvent>());
+            Assert.That(await TestHarness.Published.Any<WalletBalanceReservedIntegrationEvent>());
+
+            var wallet = await WalletRepository.SingleAsync(x => x.Id == fakeWallet.Id);
+
+            wallet.ReservedBalance.Should().Be(200);
+            wallet.Balance.Should().Be(800);
+        }
+
+        [Test]
+        public async Task Should_publish_wallet_balance_reservation_faild_when_wallet_balance_is_insufficient()
+        {
+            var fakeWallet = await CreateWalletAsync(balance: 200);
+
+            var message = new ReserveWalletBalanceIntegrationEvent()
+            {
+                TransferId = Guid.NewGuid().ToString(),
+                WalletId = fakeWallet.Id,
+                Amount = 300
+            };
+
+            await TestHarness.Bus.Publish(message);
+
+            Assert.That(await TestHarness.Consumed.Any<ReserveWalletBalanceIntegrationEvent>());
+            Assert.That(await TestHarness.Published.Any<WalletBalanceReservationFailedIntegrationEvent>());
+        }
+    }
+}
---
+++ tests/Modules/Accounting/Nexa.Accounting.Application.Tests/Wallets/WalletTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.Accounting.Domain.Wallets;
+
+namespace Nexa.Accounting.Application.Tests.Wallets
+{
+    public class WalletTestFixture : AccountingTestFixture
+    {
+        protected IWalletRepository WalletRepository { get; }
+
+        public WalletTestFixture()
+        {
+            WalletRepository = ServiceProvider.GetRequiredService<IWalletRepository>();
+        }
+
+        protected override async Task InitializeAsync(IServiceProvider services)
+        {
+            await base.InitializeAsync(services);
+
+            await TestHarness.Start();
+        }
+
+        protected override async Task ShutdownAsync(IServiceProvider services)
+        {
+            await base.ShutdownAsync(services);
+
+            await TestHarness.Stop();
+        }
+        public async Task<Wallet> CreateWalletAsync(string? userId = null , decimal balance = 0)
+        {
+            return await WithScopeAsync(async (sp) =>
+            {
+                var repository = sp.GetRequiredService<IWalletRepository>();
+
+                var wallet = new Wallet(Guid.NewGuid().ToString(), userId ?? Guid.NewGuid().ToString(), balance);
+
+                return await repository.InsertAsync(wallet);
+            });
+        }
+    }
+}
