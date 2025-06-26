7bb3d2482e43611417447e9da83a050f3d407753
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-06-26 18:13:51 +0300

    feat : Add Security services registeration

---
+++ src/Libraries/Nexa.BuildingBlocks.Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Security;
+
+namespace Nexa.BuildingBlocks.Infrastructure.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection RegisterSecurityService(this IServiceCollection services)
+        {
+            return services.AddTransient<IApplicationAuthorizationService, ApplicationAuthorizationService>();
+        }
+    }
+}
