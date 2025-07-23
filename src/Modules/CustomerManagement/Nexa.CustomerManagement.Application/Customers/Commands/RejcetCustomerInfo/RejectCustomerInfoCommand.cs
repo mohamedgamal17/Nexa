c3e7bab946934726b5319a2b3bb0e4493647a6c0
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-07-23 22:32:55 +0300

    feat(customer-management) : Add reject customer info command -Reject customer info verification from 3rd party provider

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/RejcetCustomerInfo/RejectCustomerInfoCommand.cs
@@ -0,0 +1,9 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+
+namespace Nexa.CustomerManagement.Application.Customers.Commands.RejcetCustomerInfo
+{
+    public class RejectCustomerInfoCommand : ICommand
+    {
+        public string KycCustomerId { get; set; }
+    }
+}
