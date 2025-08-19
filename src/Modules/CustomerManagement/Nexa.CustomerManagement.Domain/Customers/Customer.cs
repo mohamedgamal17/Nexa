c825cff4d6dd344165fd641b371bb1e45ede2736
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-19 16:55:52 +0300

    feat(customer-managment) : Add additional state property to customer entity to track customer verification

--- src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/Customer.cs
@@ -13,7 +13,7 @@ namespace Nexa.CustomerManagement.Domain.Customers
         public string EmailAddress { get; set; }
         public CustomerInfo? Info { get; set; }
         public Document? Document { get; set; }
-
+        public VerificationState State { get; private set; }
         private Customer()
         {
             
