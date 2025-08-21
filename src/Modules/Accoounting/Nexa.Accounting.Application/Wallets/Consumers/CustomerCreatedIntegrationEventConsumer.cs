18898fb232d99a5094f5dca389124f171a155e2d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-22 02:31:02 +0300

    feat(accounting) : Implement CustomerCreatedIntegrationEventConsumer to auto create customer wallet after customer creation

--- src/Modules/Accoounting/Nexa.Accounting.Application/Wallets/Consumers/CustomerCreatedIntegrationEventConsumer.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Wallets/Consumers/CustomerCreatedIntegrationEventConsumer.cs
@@ -1,6 +1,32 @@
-﻿namespace Nexa.Accounting.Application.Wallets.Consumers
+﻿using MassTransit;
+using MediatR;
+using Nexa.Accounting.Application.Wallets.Commands.CreateWallet;
+using Nexa.CustomerManagement.Shared.Events;
+namespace Nexa.Accounting.Application.Wallets.Consumers
 {
-    public class CustomerCreatedIntegrationEventConsumer
+    public class CustomerCreatedIntegrationEventConsumer : IConsumer<CustomerCreatedIntegrationEvent>
     {
+        private readonly IMediator _mediator;
+
+        public CustomerCreatedIntegrationEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<CustomerCreatedIntegrationEvent> context)
+        {
+            var command = new CreateWalletCommand
+            {
+                UserId = context.Message.UserId,
+                CustomerId = context.Message.CustomerId
+            };
+
+            var result = await _mediator.Send(command);
+
+            if (result.IsFailure)
+            {
+                throw result.Exception!;
+            }
+        }
     }
 }
