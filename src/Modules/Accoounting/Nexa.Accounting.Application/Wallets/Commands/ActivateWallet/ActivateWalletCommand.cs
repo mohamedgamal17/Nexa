96532e20095a2dd543ef41696f1b35f2d02fe6f9
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-22 02:54:16 +0300

    feat(accounting) : Add Activate customer wallet command

---
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Wallets/Commands/ActivateWallet/ActivateWalletCommand.cs
@@ -0,0 +1,10 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+
+namespace Nexa.Accounting.Application.Wallets.Commands.ActivateWallet
+{
+    public class ActivateWalletCommand : ICommand
+    {
+        public string CustomerId { get; set; }
+        public string FintechId { get; set; }
+    }
+}
