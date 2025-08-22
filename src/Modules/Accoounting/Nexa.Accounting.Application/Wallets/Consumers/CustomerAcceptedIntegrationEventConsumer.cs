d1175d000109913ff421402f9502590636a7dd4e
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-22 15:59:55 +0300

    feat(accounting) : Add CustomerAcceptedIntegrationEvent consumer

---
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Wallets/Consumers/CustomerAcceptedIntegrationEventConsumer.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using MediatR;
+using Nexa.Accounting.Application.Wallets.Commands.ActivateWallet;
+using Nexa.CustomerManagement.Shared.Events;
+
+namespace Nexa.Accounting.Application.Wallets.Consumers
+{
+    public class CustomerAcceptedIntegrationEventConsumer : IConsumer<CustomerAcceptedIntegrationEvent>
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerAcceptedIntegrationEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<CustomerAcceptedIntegrationEvent> context)
+        {
+            var command = new ActivateWalletCommand
+            {
+                FintechCustomerId = context.Message.FintechCustomerId,
+                CustomerId = context.Message.CustomerId
+            };
+
+            var result = await _mediator.Send(command);
+
+            if (result.IsFailure)
+            {
+                throw result.Exception!;
+            }
+        }
+    }
+}
