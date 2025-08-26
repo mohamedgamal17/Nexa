63da0ae4b1a247e6b2545c91194dd6181afe4ee2
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-26 03:23:00 +0300

    feat(accounting) : Add BankAccount domain entity efcore configuration and migration

---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/EntityFramework/Configurations/BankAccountEntityTypeConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nexa.Accounting.Domain.FundingResources;
+
+namespace Nexa.Accounting.Infrastructure.EntityFramework.Configurations
+{
+    public class BankAccountEntityTypeConfiguration : IEntityTypeConfiguration<BankAccount>
+    {
+        public void Configure(EntityTypeBuilder<BankAccount> builder)
+        {
+            builder.ToTable(BankAccountTableConsts.TableName);
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasMaxLength(BankAccountTableConsts.IdLength);
+
+            builder.Property(x => x.UserId).HasMaxLength(BankAccountTableConsts.UserIdLength);
+
+            builder.Property(x => x.CustomerId).HasMaxLength(BankAccountTableConsts.CustomerIdLength);
+
+            builder.Property(x => x.ProviderBankAccountId).HasMaxLength(BankAccountTableConsts.ProviderBankAccountIdLength);
+
+            builder.Property(x => x.HolderName).HasMaxLength(BankAccountTableConsts.HolderNameLength);
+
+            builder.Property(x => x.BankName).HasMaxLength(BankAccountTableConsts.BankNameLength);
+
+            builder.Property(x => x.Country).HasMaxLength(BankAccountTableConsts.CountryLength);
+
+            builder.Property(x => x.Currency).HasMaxLength(BankAccountTableConsts.CurrencyLength);
+
+            builder.Property(x => x.RoutingNumber).HasMaxLength(BankAccountTableConsts.RoutingNumberLength);
+
+            builder.Property(x => x.AccountNumberLast4).HasMaxLength(BankAccountTableConsts.AccountNumberLast4Length);
+
+            builder.HasIndex(x => x.UserId);
+
+            builder.HasIndex(x => x.CustomerId);
+
+            builder.HasIndex(x => x.ProviderBankAccountId);
+
+        }
+    }
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Infrastructure/EntityFramework/Migrations/20250826002009_BankAccountMigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexa.Accounting.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class BankAccountMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BanKAccounts",
+                schema: "Accounting",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    CustomerId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ProviderBankAccountId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    HolderName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    AccountNumberLast4 = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    RoutingNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BanKAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BanKAccounts_CustomerId",
+                schema: "Accounting",
+                table: "BanKAccounts",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BanKAccounts_ProviderBankAccountId",
+                schema: "Accounting",
+                table: "BanKAccounts",
+                column: "ProviderBankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BanKAccounts_UserId",
+                schema: "Accounting",
+                table: "BanKAccounts",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BanKAccounts",
+                schema: "Accounting");
+        }
+    }
+}
