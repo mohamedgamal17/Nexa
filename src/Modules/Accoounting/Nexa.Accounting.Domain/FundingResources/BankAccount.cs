fa021fd542eb3f3477067515d45131be6c8f2e4b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-08-26 03:22:35 +0300

    feat(accounting) : Add BankAccount domain entity

---
+++ src/Modules/Accoounting/Nexa.Accounting.Domain/FundingResources/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using Nexa.BuildingBlocks.Domain;
+
+namespace Nexa.Accounting.Domain.FundingResources
+{
+    public class BankAccount : BaseEntity
+    {
+     
+        public string UserId { get; private set; }
+        public string CustomerId { get; private set; }
+        public string ProviderBankAccountId { get; private set; }
+        public string HolderName { get; private set; }
+        public string BankName { get; private set; }
+        public string Country { get; private set; }
+        public string Currency { get; private set; }
+        public string AccountNumberLast4 { get; private set; }
+        public string RoutingNumber { get; private set; }
+        private BankAccount() { }
+        public BankAccount(string userId, string customerId, string providerBankAccountId, string holderName, string bankName, string country, string currency, string accountNumberLast4, string routingNumber)
+        {
+            UserId = userId;
+            CustomerId = customerId;
+            ProviderBankAccountId = providerBankAccountId;
+            HolderName = holderName;
+            BankName = bankName;
+            Country = country;
+            Currency = currency;
+            AccountNumberLast4 = accountNumberLast4;
+            RoutingNumber = routingNumber;
+        }
+
+    }
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Domain/FundingResources/BankAccountTableConsts.cs
@@ -0,0 +1,27 @@
+﻿namespace Nexa.Accounting.Domain.FundingResources
+{
+    public class BankAccountTableConsts
+    {
+        public const string TableName = "BanKAccounts";
+
+        public const int IdLength = 256;
+
+        public const int UserIdLength = 256;
+
+        public const int CustomerIdLength = 256;
+
+        public const int ProviderBankAccountIdLength = 256;
+
+        public const int HolderNameLength = 100;
+
+        public const int BankNameLength = 100;
+
+        public const int CountryLength = 3;
+
+        public const int CurrencyLength = 3;
+
+        public const int AccountNumberLast4Length = 4;
+
+        public const int RoutingNumberLength = 20;
+    }
+}
