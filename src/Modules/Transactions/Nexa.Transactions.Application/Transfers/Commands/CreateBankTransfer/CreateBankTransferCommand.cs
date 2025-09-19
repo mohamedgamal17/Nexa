18b8c8afc377e4eb5b2366d87d915d05c22e124c
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-20 02:24:24 +0300

    feat(transfer) : Add CreateBankTransferCommand

---
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Commands/CreateBankTransfer/CreateBankTransferCommand.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Shared.Services;
+using Nexa.Integrations.Baas.Abstractions.Services;
+using Nexa.Transactions.Application.Transfers.Dtos;
+using Nexa.Transactions.Shared.Enums;
+
+namespace Nexa.Transactions.Application.Transfers.Commands.CreateBankTransfer
+{
+    [Authorize]
+    public class CreateBankTransferCommand : ICommand<TransferDto>
+    {
+        public string WalletId { get; set; }
+        public string FundingResourceId { get; set; }
+        public decimal Amount { get; set; }
+        public TransferDirection Direction { get; set; }
+    }
+}
---
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Commands/CreateBankTransfer/CreateBankTransferCommandHandler.cs
@@ -0,0 +1,96 @@
+﻿using Nexa.Accounting.Application.FundingResources.Dtos;
+using Nexa.Accounting.Shared.Dtos;
+using Nexa.Accounting.Shared.Enums;
+using Nexa.Accounting.Shared.Services;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.BuildingBlocks.Domain.Exceptions;
+using Nexa.BuildingBlocks.Domain.Results;
+using Nexa.Transactions.Application.Transfers.Dtos;
+using Nexa.Transactions.Application.Transfers.Factories;
+using Nexa.Transactions.Application.Transfers.Services;
+using Nexa.Transactions.Domain.Transfers;
+using Nexa.Transactions.Shared.Enums;
+
+namespace Nexa.Transactions.Application.Transfers.Commands.CreateBankTransfer
+{
+    public class CreateBankTransferCommandHandler : IApplicationRequestHandler<CreateBankTransferCommand, TransferDto>
+    {
+        private readonly IWalletService _walletService;
+        private readonly IFundingResourceService _fundingResourceService;
+        private readonly ITransferRepository _transferRepository;
+        private readonly ITransferNumberGeneratorService _transferNumberGenerator;
+        private readonly ITransactionResponseFactory _transactionResponseFactory;
+        private readonly ISecurityContext _securityContext;
+
+        public CreateBankTransferCommandHandler(IWalletService walletService, IFundingResourceService fundingResourceService, ITransferRepository transferRepository, ITransferNumberGeneratorService transferNumberGenerator, ITransactionResponseFactory transactionResponseFactory, ISecurityContext securityContext)
+        {
+            _walletService = walletService;
+            _fundingResourceService = fundingResourceService;
+            _transferRepository = transferRepository;
+            _transferNumberGenerator = transferNumberGenerator;
+            _transactionResponseFactory = transactionResponseFactory;
+            _securityContext = securityContext;
+        }
+
+        public async Task<Result<TransferDto>> Handle(CreateBankTransferCommand request, CancellationToken cancellationToken)
+        {
+            string userId = _securityContext.User!.Id!;
+
+            var wallet = await _walletService.GetWalletById(request.WalletId);
+
+            if (wallet == null)
+            {
+                return new Result<TransferDto>(new BusinessLogicException("User wallet is not exist."));
+            }
+
+            if(!IsWalletOwner(wallet, userId))
+            {
+                return new Result<TransferDto>(new ForbiddenAccessException("Current user dosen't own this wallet."));
+            }
+
+            if(wallet.State == WalletState.Frozen)
+            {
+                return new Result<TransferDto>(new BusinessLogicException("Current wallet is frozen operation cannot be done."));
+            }
+
+            var fundingResource = await _fundingResourceService.GetFundingResourceById(request.FundingResourceId);
+
+            if(fundingResource == null)
+            {
+                return new Result<TransferDto>(new BusinessLogicException("Funding resource is not exist."));
+            }
+
+            if (!IsFundingResourceOwner(fundingResource, userId))
+            {
+                return new Result<TransferDto>(new ForbiddenAccessException("Current user dosen't own this funding resource."));
+            }
+
+            string transferNumber = _transferNumberGenerator.Generate();
+
+            BankTransfer transfer;
+
+            if(request.Direction == TransferDirection.Credit)
+            {
+                transfer = BankTransfer.Deposit(userId, request.WalletId, transferNumber, request.Amount, request.FundingResourceId);
+            }
+            else
+            {
+                transfer = BankTransfer.Withdraw(userId, request.WalletId, transferNumber, request.Amount, request.FundingResourceId);
+            }
+
+             
+            await _transferRepository.InsertAsync(transfer);
+
+
+            var view = await _transferRepository.GetViewByIdAsync(transfer.Id);
+
+            return await _transactionResponseFactory.PrepareDto(view);
+
+        }
+
+        private bool IsWalletOwner(WalletDto wallet, string userId) => wallet.UserId == userId;
+
+        private bool IsFundingResourceOwner(BankAccountDto bankAccountDto, string userId) => bankAccountDto.UserId == userId;
+    }
+}
