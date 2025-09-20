29bb006d2dc85cd7c458b36822b7d028bb34450f
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-21 00:20:57 +0300

    feat(transfer) : Add ReciveBalanceIntegrationEvent with it's consumer

---
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Wallets/Consumers/ReciveBalanceIntegrationEventConsumer.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Nexa.Accounting.Domain;
+using Nexa.Accounting.Domain.Enums;
+using Nexa.Accounting.Domain.Wallets;
+using Nexa.Accounting.Shared.Events;
+
+namespace Nexa.Accounting.Application.Wallets.Consumers
+{
+    public class ReciveBalanceIntegrationEventConsumer : IConsumer<ReciveBalanceIntegrationEvent>
+    {
+        private readonly IWalletRepository _walletRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IAccountingRepository<LedgerEntry> _ledgerEntryRepository;
+
+        public ReciveBalanceIntegrationEventConsumer(IWalletRepository walletRepository, IPublishEndpoint publishEndpoint, IAccountingRepository<LedgerEntry> ledgerEntryRepository)
+        {
+            _walletRepository = walletRepository;
+            _publishEndpoint = publishEndpoint;
+            _ledgerEntryRepository = ledgerEntryRepository;
+        }
+
+        public async Task Consume(ConsumeContext<ReciveBalanceIntegrationEvent> context)
+        {
+            var wallet = await _walletRepository.SingleAsync(x => x.Id == context.Message.WalletId);
+
+            wallet.Credit(context.Message.Amount);
+
+            var ledgerEntry = new LedgerEntry(wallet.Id, context.Message.Amount, TransferType.Bank,
+                 TransferDirection.Credit, context.Message.TransferId, context.Message.CompletedAt);
+
+            await _walletRepository.UpdateAsync(wallet);
+
+            await _ledgerEntryRepository.InsertAsync(ledgerEntry);
+
+
+            var integrationEvent = new ReciveBalanceCompletedIntegrationEvent
+            {
+                TransferId = context.Message.TransferId,
+                WalletId = context.Message.WalletId
+            };
+
+            await _publishEndpoint.Publish(integrationEvent);
+        }
+    }
+
+}
---
+++ src/Modules/Accoounting/Nexa.Accounting.Shared/Events/ReciveBalanceIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace Nexa.Accounting.Shared.Events
+{
+    public class ReciveBalanceIntegrationEvent
+    {
+        public string WalletId { get; set; }
+        public string TransferId { get; set; }
+        public string TransferNumber { get; set; }
+        public decimal Amount { get; set; }
+        public string FundingResourceId { get; set; }
+        public DateTime CompletedAt { get; set; }
+    }
+
+    public class ReciveBalanceCompletedIntegrationEvent
+    {
+        public string WalletId { get; set; }
+        public string TransferId { get; set; }
+    }
+}
