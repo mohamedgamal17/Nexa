0f75caf488e9b89d0c7968bf7f87fa73bf1a15f1
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-20 17:46:48 +0300

    feat(transfer) : Add additional for TransferPendingEvent

--- src/Modules/Transactions/Nexa.Transactions.Domain/Events/TransferPendingEvent.cs
+++ src/Modules/Transactions/Nexa.Transactions.Domain/Events/TransferPendingEvent.cs
@@ -5,18 +5,21 @@ namespace Nexa.Transactions.Domain.Events
     public class TransferPendingEvent : IEvent
     {
         public string Id { get; }
+        public string UserId { get; }
         public string WalletId { get; }
         public string Number { get; }
         public decimal Amount { get; }
         public TransferType Type { get; }
 
         public TransferPendingEvent(string id,
+            string userId,
             string walletId,
             string transactionNumber,
             decimal amount,
             TransferType type)
         {
             Id = id;
+            UserId = userId;
             WalletId = walletId;
             Number = transactionNumber;
             Amount = amount;
