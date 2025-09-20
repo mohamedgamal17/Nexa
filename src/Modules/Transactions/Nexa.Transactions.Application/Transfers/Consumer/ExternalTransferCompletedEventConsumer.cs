35a0bd36e990861edf3e873747586e1e397f0d49
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-21 00:22:04 +0300

    feat(transfer) : Add ExternalTransferCompletedIntegrationEvent with it's consumer

---
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ExternalTransferCompletedEventConsumer.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Domain.Transfers;
+using Nexa.Transactions.Shared.Events;
+
+namespace Nexa.Transactions.Application.Transfers.Consumer
+{
+    public class ExternalTransferCompletedEventConsumer : IConsumer<ExternalTransferCompletedIntegrationEvent>
+    {
+        private readonly ITransferRepository _transferRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public ExternalTransferCompletedEventConsumer(ITransferRepository transferRepository, IPublishEndpoint publishEndpoint)
+        {
+            _transferRepository = transferRepository;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<ExternalTransferCompletedIntegrationEvent> context)
+        {
+            var transfer = await _transferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
+
+            if(transfer is BankTransfer bankTransfer)
+            {
+                if(bankTransfer.Direction == Shared.Enums.TransferDirection.Credit)
+                {
+                    var @event = new ReciveBalanceIntegrationEvent
+                    {
+                        WalletId = bankTransfer.WalletId,
+                        TransferId = bankTransfer.Id,
+                        TransferNumber = bankTransfer.Number,
+                        FundingResourceId = bankTransfer.FundingResourceId,
+                        Amount = bankTransfer.Amount,
+                        CompletedAt = DateTime.UtcNow
+                    };
+
+                    await _publishEndpoint.Publish(@event);
+                }
+            }
+        }
+    }
+}
---
+++ src/Modules/Transactions/Nexa.Transactions.Shared/Events/ExternalTransferCompletedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Nexa.Transactions.Shared.Events
+{
+    public class ExternalTransferCompletedIntegrationEvent
+    {
+        public string TransferId { get; set; }
+        public string ExternalTransferId { get; set; }
+    }
+}
