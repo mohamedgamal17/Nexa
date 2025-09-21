c78c008493bc337f5511149cd17765feb6dfc311
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-21 16:59:55 +0300

    feat(transfer) : Add additional UserId property to TransferNetworkFundsIntegrationEvent

--- src/Modules/Accoounting/Nexa.Accounting.Shared/Events/TransferNetworkFundsIntegrationEvent.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Shared/Events/TransferNetworkFundsIntegrationEvent.cs
@@ -2,13 +2,15 @@
 {
     public class TransferNetworkFundsIntegrationEvent
     {
+        public string UserId { get; set; }
         public string TransferId { get; set; }
         public string WalletId { get; set; }
         public string ReciverId { get; set; }
         public decimal Amount { get; set; }
 
-        public TransferNetworkFundsIntegrationEvent(string transferId, string walletId, string reciverId, decimal amount)
+        public TransferNetworkFundsIntegrationEvent(string userId,string transferId, string walletId, string reciverId, decimal amount)
         {
+            UserId = userId;
             TransferId = transferId;
             WalletId = walletId;
             ReciverId = reciverId;
