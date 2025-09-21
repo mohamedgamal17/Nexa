9d4da5576776ba796979c65f4ee5cfdcfaac19ab
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-09-21 16:58:16 +0300

    feat(transfer) : Update ProcessNetworkTransferIntegrationEventConsumer to sync network transfer on external system

--- src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ProcessNetworkTransferIntegrationEventConsumer.cs
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ProcessNetworkTransferIntegrationEventConsumer.cs
@@ -1,22 +1,54 @@
 ﻿using MassTransit;
 using Nexa.Accounting.Shared.Events;
+using Nexa.Accounting.Shared.Services;
+using Nexa.CustomerManagement.Shared.Services;
+using Nexa.Integrations.Baas.Abstractions.Contracts.Transfers;
+using Nexa.Integrations.Baas.Abstractions.Services;
+using Nexa.Transactions.Domain;
+using Nexa.Transactions.Domain.Transfers;
 using Nexa.Transactions.Shared.Events;
 namespace Nexa.Transactions.Application.Transfers.Consumer
 {
     public class ProcessNetworkTransferIntegrationEventConsumer : IConsumer<ProcessNetworkTransferIntegrationEvent>
     {
-        private readonly IPublishEndpoint _publishEndpoint;
 
-        public ProcessNetworkTransferIntegrationEventConsumer(IPublishEndpoint publishEndpoint)
+        private readonly ITransactionRepository<NetworkTransfer> _networkTransferRepository;
+        private readonly ICustomerService _customerService;
+        private readonly IWalletService _walletService;
+        private readonly IBaasTransferService _baasTransferService;
+
+        public ProcessNetworkTransferIntegrationEventConsumer(ITransactionRepository<NetworkTransfer> networkTransferRepository, ICustomerService customerService, IWalletService walletService, IBaasTransferService baasTransferService)
         {
-            _publishEndpoint = publishEndpoint;
+            _networkTransferRepository = networkTransferRepository;
+            _customerService = customerService;
+            _walletService = walletService;
+            _baasTransferService = baasTransferService;
         }
 
         public async Task Consume(ConsumeContext<ProcessNetworkTransferIntegrationEvent> context)
         {
-            var message = new TransferNetworkFundsIntegrationEvent(context.Message.TransferId, context.Message.WalletId, context.Message.ReciverId, context.Message.Amount);
+            var transfer = await _networkTransferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
 
-            await _publishEndpoint.Publish(message);
+            var senderCustomer = await _customerService.GetCustomerByUserId(transfer.UserId);
+
+            var senderWallet = await _walletService.GetWalletById(context.Message.WalletId);
+
+            var reciverWallet = await _walletService.GetWalletById(context.Message.ReciverId);
+
+            var networkTransferRequest = new NetworkTransferRequest
+            {
+                ClientTransferId = transfer.Id,
+                SenderAccountId = senderCustomer!.FintechCustomerId!,
+                SenderWalletId = senderWallet!.ProviderWalletId!,
+                ReciverWalletId = reciverWallet!.Id,
+                Amount = context.Message.Amount
+            };
+
+            var baasTransfer = await _baasTransferService.NetworkTransfer(networkTransferRequest);
+
+            transfer.AssignExternalTransferId(baasTransfer.Id);
+
+            await _networkTransferRepository.UpdateAsync(transfer);
         }
     }
 }
