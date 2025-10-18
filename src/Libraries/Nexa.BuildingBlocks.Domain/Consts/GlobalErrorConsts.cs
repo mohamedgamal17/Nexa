db26f8cf628a8d31069a278091061b5f725753a0
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-18 20:40:04 +0300

    refactor : Remove ResourceNotOwned error type from global  errors

--- src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
+++ src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
@@ -5,10 +5,6 @@ namespace Nexa.BuildingBlocks.Domain.Consts
 {
     public static class GlobalErrorConsts
     {
-        public static NexaError ResourceNotFound
-            => new(nameof(ResourceNotFound).ToCamelCase(), "The specified resource could not be found.");
-        public static NexaError ForbiddenAccess
-            => new(nameof(ForbiddenAccess).ToCamelCase(), "You don't have permission to access this resource.");
 
         public static NexaError UnauthorizedAccess
             = new(nameof(UnauthorizedAccess).ToCamelCase(), "You are not authorized to access this resource.");
