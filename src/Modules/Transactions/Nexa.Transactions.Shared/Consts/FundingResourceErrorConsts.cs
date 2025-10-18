a81265dafbe54f2813bdaab611bad1340eba853d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-18 20:29:18 +0300

    refactor : Add transactions module base errors

---
+++ src/Modules/Transactions/Nexa.Transactions.Shared/Consts/FundingResourceErrorConsts.cs
@@ -0,0 +1,14 @@
+﻿using Minerals.StringCases;
+using Nexa.BuildingBlocks.Domain.Exceptions;
+
+namespace Nexa.Transactions.Shared.Consts
+{
+    public class FundingResourceErrorConsts
+    {
+        public static NexaError FundingResourceNotExist
+            => new(nameof(FundingResourceNotExist).ToCamelCase(), "Funding resource is not exist.");
+
+        public static NexaError FundingResourceNotOwned
+            => new(nameof(FundingResourceNotOwned).ToCamelCase(), "The funding resource does not belong to the current user.");
+    }
+}
---
+++ src/Modules/Transactions/Nexa.Transactions.Shared/Consts/TransferErrorConsts.cs
@@ -0,0 +1,11 @@
+﻿using Minerals.StringCases;
+using Nexa.BuildingBlocks.Domain.Exceptions;
+
+namespace Nexa.Transactions.Shared.Consts
+{
+    public class TransferErrorConsts
+    {
+        public static NexaError TransferNotExist
+            => new(nameof(TransferNotExist).ToCamelCase(), "Transfer is not exist.");
+    }
+}
