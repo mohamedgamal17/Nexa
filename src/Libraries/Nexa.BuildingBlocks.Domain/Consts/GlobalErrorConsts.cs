4fe1d9bb629b934fb5eb5330a9998b4e62242d8e
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-18 16:24:49 +0300

    refactor : Add string case library

--- src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
+++ src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
@@ -1,16 +1,16 @@
-﻿using Nexa.BuildingBlocks.Domain.Exceptions;
+﻿using Minerals.StringCases;
+using Nexa.BuildingBlocks.Domain.Exceptions;
 
 namespace Nexa.BuildingBlocks.Domain.Consts
 {
     public static class GlobalErrorConsts
     {
         public static NexaError ResourceNotFound
-            => new("resoruceNotFound", "The specified {0} could not be found.");
-
+            => new(nameof(ResourceNotFound).ToCamelCase(), "The specified {0} could not be found.");
         public static NexaError ForbiddenAccess
-            => new("forbiddenAccess", "You don't have permission to access this resource.");
+            => new(nameof(ForbiddenAccess).ToCamelCase(), "You don't have permission to access this resource.");
 
         public static NexaError UnauthorizedAccess
-            = new("unauthorizedAccess", "You are not authorized to access this resource.");
+            = new(nameof(UnauthorizedAccess).ToCamelCase(), "You are not authorized to access this resource.");
     }
 }
