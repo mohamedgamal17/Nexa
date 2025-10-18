a1c717159b2189582802c4b56e7423b9ff1c21ee
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-18 21:16:10 +0300

    refactor : Define custom NexaUnauthorizedAccessException

---
+++ src/Libraries/Nexa.BuildingBlocks.Domain/Exceptions/NexaUnauthorizedAccessException.cs
@@ -0,0 +1,16 @@
+﻿namespace Nexa.BuildingBlocks.Domain.Exceptions
+{
+    public class NexaUnauthorizedAccessException : NexaException
+    {
+        public NexaUnauthorizedAccessException()
+        {
+
+        }
+
+        public NexaUnauthorizedAccessException(string code, string? message = null) : base(message)
+        {
+        }
+
+        public NexaUnauthorizedAccessException(NexaError error, List<object>? data = null) : base(error) { }
+    }
+}
