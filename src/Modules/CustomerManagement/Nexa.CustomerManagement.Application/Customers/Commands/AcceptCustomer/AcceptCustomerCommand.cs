e83894cee954404f799dd4642bdd04250af28ae9
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-20 04:17:16 +0300

    refactor(customer-management) : Remove AcceptCustomerCommand

--- src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/AcceptCustomer/AcceptCustomerCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nexa.BuildingBlocks.Application.Requests;
-
-namespace Nexa.CustomerManagement.Application.Customers.Commands.AcceptCustomer
-{
-    public class AcceptCustomerCommand : ICommand
-    {
-        public string FintechCustomerId { get; set; }
-    }
-}
