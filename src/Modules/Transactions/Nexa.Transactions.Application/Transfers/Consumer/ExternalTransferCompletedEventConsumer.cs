40e662d0471a54c7ebba613231c5bd89c5ea9d10
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-10-21 15:29:56 +0300

    refactor(transfers) : Remove external network transfer creation logic

--- src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ExternalTransferCompletedEventConsumer.cs
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ExternalTransferCompletedEventConsumer.cs
@@ -1,68 +1,51 @@
 ﻿using MassTransit;
 using Nexa.Accounting.Shared.Events;
+using Nexa.Transactions.Domain;
 using Nexa.Transactions.Domain.Transfers;
 using Nexa.Transactions.Shared.Events;
-
 namespace Nexa.Transactions.Application.Transfers.Consumer
 {
     public class ExternalTransferCompletedEventConsumer : IConsumer<ExternalTransferCompletedIntegrationEvent>
     {
-        private readonly ITransferRepository _transferRepository;
+        private readonly ITransactionRepository<BankTransfer> _bankAccountTransferRepository;
         private readonly IPublishEndpoint _publishEndpoint;
 
-        public ExternalTransferCompletedEventConsumer(ITransferRepository transferRepository, IPublishEndpoint publishEndpoint)
+        public ExternalTransferCompletedEventConsumer(ITransactionRepository<BankTransfer> bankAccountTransferRepository, IPublishEndpoint publishEndpoint)
         {
-            _transferRepository = transferRepository;
+            _bankAccountTransferRepository = bankAccountTransferRepository;
             _publishEndpoint = publishEndpoint;
         }
 
         public async Task Consume(ConsumeContext<ExternalTransferCompletedIntegrationEvent> context)
         {
-            var transfer = await _transferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
+            var bankTransfer = await _bankAccountTransferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
 
-            if(transfer is BankTransfer bankTransfer)
+            if (bankTransfer.Direction == Shared.Enums.TransferDirection.Credit)
             {
-                if(bankTransfer.Direction == Shared.Enums.TransferDirection.Credit)
+                var @event = new ReciveBalanceIntegrationEvent
                 {
-                    var @event = new ReciveBalanceIntegrationEvent
-                    {
-                        WalletId = bankTransfer.WalletId,
-                        TransferId = bankTransfer.Id,
-                        TransferNumber = bankTransfer.Number,
-                        FundingResourceId = bankTransfer.FundingResourceId,
-                        Amount = bankTransfer.Amount,
-                        CompletedAt = DateTime.UtcNow
-                    };
+                    WalletId = bankTransfer.WalletId,
+                    TransferId = bankTransfer.Id,
+                    TransferNumber = bankTransfer.Number,
+                    FundingResourceId = bankTransfer.FundingResourceId,
+                    Amount = bankTransfer.Amount,
+                    CompletedAt = DateTime.UtcNow
+                };
 
-                    await _publishEndpoint.Publish(@event);
-                }
-                else
-                {
-
-                    var @event = new TransferFundsIntegrationEvent
-                    {
-                        WalletId = bankTransfer.WalletId,
-                        TransferId = bankTransfer.Id,
-                        TransferNumber = bankTransfer.Number,
-                        FundingResourceId = bankTransfer.FundingResourceId,
-                        Amount = bankTransfer.Amount,
-                        CompletedAt = DateTime.UtcNow
-                    };
-
-                    await _publishEndpoint.Publish(@event);
-                }
+                await _publishEndpoint.Publish(@event);
             }
-
-            if(transfer is NetworkTransfer networkTransfer)
+            else
             {
-                var @event = new TransferNetworkFundsIntegrationEvent
-                    (
-                        networkTransfer.UserId,
-                        networkTransfer.Id,
-                        networkTransfer.WalletId,
-                        networkTransfer.ReciverId,
-                        networkTransfer.Amount
-                    );
+
+                var @event = new TransferFundsIntegrationEvent
+                {
+                    WalletId = bankTransfer.WalletId,
+                    TransferId = bankTransfer.Id,
+                    TransferNumber = bankTransfer.Number,
+                    FundingResourceId = bankTransfer.FundingResourceId,
+                    Amount = bankTransfer.Amount,
+                    CompletedAt = DateTime.UtcNow
+                };
 
                 await _publishEndpoint.Publish(@event);
             }
--- src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ProcessNetworkTransferIntegrationEventConsumer.cs
+++ src/Modules/Transactions/Nexa.Transactions.Application/Transfers/Consumer/ProcessNetworkTransferIntegrationEventConsumer.cs
@@ -1,9 +1,5 @@
 ﻿using MassTransit;
 using Nexa.Accounting.Shared.Events;
-using Nexa.Accounting.Shared.Services;
-using Nexa.CustomerManagement.Shared.Services;
-using Nexa.Integrations.Baas.Abstractions.Contracts.Transfers;
-using Nexa.Integrations.Baas.Abstractions.Services;
 using Nexa.Transactions.Domain;
 using Nexa.Transactions.Domain.Transfers;
 using Nexa.Transactions.Shared.Events;
@@ -13,42 +9,30 @@ namespace Nexa.Transactions.Application.Transfers.Consumer
     {
 
         private readonly ITransactionRepository<NetworkTransfer> _networkTransferRepository;
-        private readonly ICustomerService _customerService;
-        private readonly IWalletService _walletService;
-        private readonly IBaasTransferService _baasTransferService;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public ProcessNetworkTransferIntegrationEventConsumer(ITransactionRepository<NetworkTransfer> networkTransferRepository, ICustomerService customerService, IWalletService walletService, IBaasTransferService baasTransferService)
+        public ProcessNetworkTransferIntegrationEventConsumer(ITransactionRepository<NetworkTransfer> networkTransferRepository, IPublishEndpoint publishEndpoint)
         {
             _networkTransferRepository = networkTransferRepository;
-            _customerService = customerService;
-            _walletService = walletService;
-            _baasTransferService = baasTransferService;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task Consume(ConsumeContext<ProcessNetworkTransferIntegrationEvent> context)
         {
-            var transfer = await _networkTransferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
+            var networkTransfer = await _networkTransferRepository.SingleAsync(x => x.Id == context.Message.TransferId);
 
-            var senderCustomer = await _customerService.GetCustomerByUserId(transfer.UserId);
 
-            var senderWallet = await _walletService.GetWalletById(context.Message.WalletId);
+            var @event = new TransferNetworkFundsIntegrationEvent
+                    (
+                        networkTransfer.UserId,
+                        networkTransfer.Id,
+                        networkTransfer.WalletId,
+                        networkTransfer.ReciverId,
+                        networkTransfer.Amount
+                    );
 
-            var reciverWallet = await _walletService.GetWalletById(context.Message.ReciverId);
-
-            var networkTransferRequest = new NetworkTransferRequest
-            {
-                ClientTransferId = transfer.Id,
-                SenderAccountId = senderCustomer!.FintechCustomerId!,
-                SenderWalletId = senderWallet!.ProviderWalletId!,
-                ReciverWalletId = reciverWallet!.ProviderWalletId,
-                Amount = context.Message.Amount
-            };
-
-            var baasTransfer = await _baasTransferService.NetworkTransfer(networkTransferRequest);
-
-            transfer.AssignExternalTransferId(baasTransfer.Id);
-
-            await _networkTransferRepository.UpdateAsync(transfer);
+      
+            await _publishEndpoint.Publish(@event);           
         }
     }
 }
