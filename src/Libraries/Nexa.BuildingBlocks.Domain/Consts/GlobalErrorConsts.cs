a9266bb83759bde7e9bf8bbf05b400dacf55860b
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-11-17 04:00:13 +0200

    refactor : Add global validaton errors

--- src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
+++ src/Libraries/Nexa.BuildingBlocks.Domain/Consts/GlobalErrorConsts.cs
@@ -10,5 +10,26 @@ namespace Nexa.BuildingBlocks.Domain.Consts
 
         public static NexaError UnauthorizedAccess
             => new(nameof(UnauthorizedAccess).ToCamelCase(), "You are not authorized to access this resource.");
+
+        public static NexaError Required
+            = new(nameof(Required).ToCamelCase(), "This field is required.");
+
+        public static NexaError MinLength
+            = new(nameof(MinLength).ToCamelCase(), "Minimum length is {0} characters.");
+
+        public static NexaError MaxLength
+            = new NexaError(nameof(MaxLength).ToCamelCase(), "Maximum length is {0} characters.");
+
+        public static NexaError InvalidPhoneNumber
+            = new NexaError(nameof(InvalidPhoneNumber).ToCamelCase(), "Invalid phone number");
+
+        public static NexaError InvalidEmailAddress
+            = new(nameof(InvalidEmailAddress).ToCamelCase(), "Email must be in a valid format");
+
+        public static NexaError InvalidBirthDate
+            = new(nameof(InvalidBirthDate).ToCamelCase(), "Minimum age requirement is 18.");
+
+        public static NexaError InvalidCountryCode
+            = new(nameof(InvalidCountryCode).ToCamelCase(), "Invalid country code.");
     }
 }
