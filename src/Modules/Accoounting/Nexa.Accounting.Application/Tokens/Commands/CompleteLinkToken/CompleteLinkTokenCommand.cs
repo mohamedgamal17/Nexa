945c907f868e2a2322a50028f004af65991ba1fb
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2025-11-17 17:44:31 +0200

    feat(accounting) : Add token commands validators

--- src/Modules/Accoounting/Nexa.Accounting.Application/Tokens/Commands/CompleteLinkToken/CompleteLinkTokenCommand.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Tokens/Commands/CompleteLinkToken/CompleteLinkTokenCommand.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Nexa.Accounting.Shared.Dtos;
 using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.BuildingBlocks.Domain.Consts;
 
 namespace Nexa.Accounting.Application.Tokens.Commands.CompleteLinkToken
 {
@@ -8,5 +10,19 @@ namespace Nexa.Accounting.Application.Tokens.Commands.CompleteLinkToken
     public class CompleteLinkTokenCommand : ICommand<BankAccountDto>
     {
         public string Token { get; set; }
+    }
+
+    public class CompleteLinkTokenCommandValidator : AbstractValidator<CompleteLinkTokenCommand>
+    {
+        public CompleteLinkTokenCommandValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotNull()
+                .WithErrorCode(GlobalErrorConsts.Required.Code)
+                .WithMessage(GlobalErrorConsts.Required.Message)
+                .MaximumLength(500)
+                .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                .WithMessage(string.Format(GlobalErrorConsts.MaxLength.Message, 500));
+        }
     }
 }
--- src/Modules/Accoounting/Nexa.Accounting.Application/Tokens/Commands/CreateLinkToken/CreateLinkTokenCommand.cs
+++ src/Modules/Accoounting/Nexa.Accounting.Application/Tokens/Commands/CreateLinkToken/CreateLinkTokenCommand.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Nexa.Accounting.Application.Tokens.Dtos;
 using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.BuildingBlocks.Domain.Consts;
 
 namespace Nexa.Accounting.Application.Tokens.Commands.CreateLinkToken
 {
@@ -8,5 +10,20 @@ namespace Nexa.Accounting.Application.Tokens.Commands.CreateLinkToken
     public class CreateLinkTokenCommand : ICommand<BankingTokenDto>
     {
         public string? RedirectUri { get; set; }
+    }
+
+    public class CreateLinkTokenCommandValidator : AbstractValidator<CreateLinkTokenCommand>
+    {
+        public CreateLinkTokenCommandValidator()
+        {
+            RuleFor(x => x.RedirectUri)
+               .Must(url => Uri.TryCreate(url, UriKind.Absolute, out _))
+               .WithErrorCode(GlobalErrorConsts.InvalidUri.Code)
+               .WithMessage(GlobalErrorConsts.InvalidUri.Message)
+               .MaximumLength(500)
+               .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+               .WithMessage(string.Format(GlobalErrorConsts.MaxLength.Message, 500))
+               .When(x => x.RedirectUri != null);
+        }
     }
 }
