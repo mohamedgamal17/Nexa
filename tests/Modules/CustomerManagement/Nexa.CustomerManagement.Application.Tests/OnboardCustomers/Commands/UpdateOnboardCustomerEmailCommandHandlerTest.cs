093977bc1f5afa314c90dd5cdbf7594b0c6e7cea
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 01:45:47 +0200

    test(customer-management): Add test cases UpdateOnboardCustomerEmailCommandHandler  onboard customer

---
+++ tests/Modules/CustomerManagement/Nexa.CustomerManagement.Application.Tests/OnboardCustomers/Commands/UpdateOnboardCustomerEmailCommandHandlerTest.cs
@@ -0,0 +1,80 @@
+﻿using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerEmail;
+using Nexa.Application.Tests.Extensions;
+using FluentAssertions;
+using Nexa.CustomerManagement.Shared.Enums;
+using Nexa.BuildingBlocks.Domain.Exceptions;
+
+namespace Nexa.CustomerManagement.Application.Tests.OnboardCustomers.Commands
+{
+    [TestFixture]
+    public class UpdateOnboardCustomerEmailCommandHandlerTest : OnboardCustomerTestFixture
+    {
+        public ICustomerManagementRepository<OnboardCustomer> OnboardCustomerRepository { get; }
+
+
+        public UpdateOnboardCustomerEmailCommandHandlerTest()
+        {
+            OnboardCustomerRepository = ServiceProvider.GetRequiredService<ICustomerManagementRepository<OnboardCustomer>>();
+        }
+
+        [Test]
+        public async Task Should_update_onboard_customer_email()
+        {
+            string userId = Guid.NewGuid().ToString();
+
+            var fakeOnboardCustomer = await CreateInitialOnboardCustomerAsync(userId);
+
+            var command = new UpdateOnboardCustomerEmailCommand
+            {
+                UserId = userId,
+                EmailAddress = Faker.Person.Email
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShouldBeSuccess();
+
+            var onboardCustomer = await OnboardCustomerRepository.SingleAsync(x => x.Id == fakeOnboardCustomer.Id);
+
+            onboardCustomer.EmailAddress.Should().Be(command.EmailAddress);
+
+        }
+
+        [Test]
+        public async Task Should_failure_while_updating_onboard_customer_when_onboard_customer_is_not_exist()
+        {
+            string userId = Guid.NewGuid().ToString();
+
+            var command = new UpdateOnboardCustomerEmailCommand
+            {
+                UserId = userId,
+                EmailAddress = Faker.Person.Email
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShoulBeFailure(typeof(EntityNotFoundException));
+        }
+
+        [Test]
+        public async Task Should_failure_while_updating_onboard_customer_when_onboard_customer_is_in_completed_state()
+        {
+            string userId = Guid.NewGuid().ToString();
+
+            await CreateCompletedOnboardCustomer(userId);
+
+            var command = new UpdateOnboardCustomerEmailCommand
+            {
+                UserId = userId,
+                EmailAddress = Faker.Person.Email
+            };
+
+            var result = await Mediator.Send(command);
+
+            result.ShoulBeFailure(typeof(BusinessLogicException));
+        }
+    }
+}
--- tests/Modules/CustomerManagement/Nexa.CustomerManagement.Application.Tests/OnboardCustomers/OnboardCustomerTestFixture.cs
+++ tests/Modules/CustomerManagement/Nexa.CustomerManagement.Application.Tests/OnboardCustomers/OnboardCustomerTestFixture.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Bogus;
+using Microsoft.Extensions.DependencyInjection;
 using Nexa.CustomerManagement.Domain;
+using Nexa.CustomerManagement.Domain.Customers;
 using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Shared.Enums;
 
 namespace Nexa.CustomerManagement.Application.Tests.OnboardCustomers
 {
     public class OnboardCustomerTestFixture : CustomerManagementTestFixture
     {
+        protected Faker Faker { get; }
 
+        public OnboardCustomerTestFixture()
+        {
+            Faker = new Faker();
+        }
         public Task<OnboardCustomer> CreateInitialOnboardCustomerAsync(string userId)
         {
             return WithScopeAsync(async (sp) =>
@@ -14,6 +22,44 @@ namespace Nexa.CustomerManagement.Application.Tests.OnboardCustomers
                 var repository = sp.GetRequiredService<ICustomerManagementRepository<OnboardCustomer>>();
 
                 var onboardCustomer = new OnboardCustomer(userId);
+
+                return await repository.InsertAsync(onboardCustomer);
+            });
+        }
+
+
+        public Task<OnboardCustomer> CreateCompletedOnboardCustomer(string userId)
+        {
+            return WithScopeAsync(async (sp) =>
+            {
+                var repository = sp.GetRequiredService<ICustomerManagementRepository<OnboardCustomer>>();
+
+                var onboardCustomer = new OnboardCustomer(userId);
+
+                onboardCustomer.UpdatePhoneNumber(Faker.Person.Phone);
+
+                onboardCustomer.UpdateEmailAddress(Faker.Person.Email);
+
+                var address = Address.Create(
+                        "US",
+                        "San",
+                        "CA",
+                        "12 CA street",
+                        "56454",
+                        "4545"
+                    );
+
+                var customerInfo = CustomerInfo.Create(
+                        Faker.Person.FirstName,
+                        Faker.Person.LastName,
+                        Faker.Person.DateOfBirth,
+                        Gender.Male,
+                        address
+                    );
+
+                onboardCustomer.UpdateCustomerInfo(customerInfo);
+
+                onboardCustomer.MarkAsCompleted();
 
                 return await repository.InsertAsync(onboardCustomer);
             });
