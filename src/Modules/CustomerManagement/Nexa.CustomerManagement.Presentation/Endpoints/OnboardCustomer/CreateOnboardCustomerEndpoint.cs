cccd0f4757b0134e668d73548229c8beef93cd45
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 22:51:12 +0200

    feat(customer-management): Add CreateOnboardCustomerEndpoint for providing user api endpoint to create onboard customer

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/CreateOnboardCustomerEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using FastEndpoints;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.CreateOnboardCustomer;
+using Nexa.CustomerManagement.Shared.Dtos;
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class CreateOnboardCustomerEndpoint : Endpoint<EmptyRequest, OnboardCustomerDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISecurityContext _securityContext;
+
+        public CreateOnboardCustomerEndpoint(IMediator mediator, ISecurityContext securityContext)
+        {
+            _mediator = mediator;
+            _securityContext = securityContext;
+        }
+
+        public override void Configure()
+        {
+            Post("");
+
+            Group<OnboardCustomerRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+        {
+            string currentUserId = _securityContext.User!.Id;
+
+            var command = new CreateOnboardCustomerCommand
+            {
+                UserId = currentUserId
+            };
+
+            var result = await _mediator.Send(command);
+
+            var responseResult =  result.ToOk();
+
+            await SendResultAsync(responseResult);
+        }   
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/OnboardCustomerRoutingGroup.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class OnboardCustomerRoutingGroup : Group
+    {
+        public OnboardCustomerRoutingGroup()
+        {
+            Configure("user/onboardcustomer", ep =>
+            {
+      
+                ep.Description(x =>
+                    x
+                    .WithTags("OnboardCustomers")
+                    .Produces(StatusCodes.Status401Unauthorized, typeof(ProblemDetails))
+                    .Produces(StatusCodes.Status403Forbidden, typeof(ProblemDetails))
+                    .Produces(StatusCodes.Status404NotFound, typeof(ProblemDetails))
+                    .Produces(StatusCodes.Status500InternalServerError, typeof(ProblemDetails)));
+            });
+        }
+    }
+}
