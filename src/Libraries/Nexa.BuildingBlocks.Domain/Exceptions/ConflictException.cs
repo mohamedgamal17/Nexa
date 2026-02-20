6df330963d9f08d255d4aa15206d9471da3c8830
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 21:39:31 +0200

    refactor : Add new global ConflictException

---
+++ src/Libraries/Nexa.BuildingBlocks.Domain/Exceptions/ConflictException.cs
@@ -0,0 +1,12 @@
+﻿namespace Nexa.BuildingBlocks.Domain.Exceptions
+{
+    public class ConflictException : NexaException
+    {
+        public ConflictException(string code, string? message = null)
+           : base(code, message)
+        {
+
+        }
+        public ConflictException(NexaError error, List<object>? data = null) : base(error, data) { }
+    }
+}
