b7b53960efadfe26ee9d7f8362b8cdfc2e68ca86
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 21:42:06 +0200

    feat(customer-management): Introduce initial CreateOnboardCustomerCommand and OnboardCustomerDto for onboarding customer flow

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Commands/CreateOnboardCustomer/CreateOnboardCustomerCommand.cs
@@ -0,0 +1,11 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Commands.CreateOnboardCustomer
+{
+    public class CreateOnboardCustomerCommand : ICommand<OnboardCustomerDto>
+    {
+        public string UserId { get; set; }
+
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Shared/Dtos/OnboardCustomerDto.cs
@@ -0,0 +1,14 @@
+﻿using Nexa.BuildingBlocks.Domain.Dtos;
+using Nexa.CustomerManagement.Shared.Enums;
+
+namespace Nexa.CustomerManagement.Shared.Dtos
+{
+    public class OnboardCustomerDto : EntityDto
+    {
+        public string  UserId { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public CustomerInfoDto? Info { get; set; }
+        public OnboardCustomerStatus Status { get; set; }
+    }
+}
