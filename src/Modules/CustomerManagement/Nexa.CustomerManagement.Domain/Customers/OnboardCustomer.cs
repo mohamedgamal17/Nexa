888eaab602ea0121de0a21cd6e800c07b8d703f8
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 19:48:40 +0200

    feat(customer-management) : Add OnboardCustomer Aggregate to simpilifiy creating  customer aggregate on the  ui

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/Customers/OnboardCustomer.cs
@@ -0,0 +1,20 @@
+﻿using Nexa.BuildingBlocks.Domain;
+using Nexa.CustomerManagement.Domain.Documents;
+using Nexa.CustomerManagement.Shared.Enums;
+
+namespace Nexa.CustomerManagement.Domain.Customers
+{
+    public class OnboardCustomer : AggregateRoot
+    {
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public CustomerInfo? Info { get; set; }
+        public Address? Address { get; set; }
+        public OnboardCustomerStatus Status { get; set; }
+
+        public OnboardCustomer()
+        {
+
+        } 
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Shared/Enums/OnboardCustomerStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Nexa.CustomerManagement.Shared.Enums
+{
+    public enum OnboardCustomerStatus
+    {
+        Started = 0,
+        EmailProvided = 5,
+        PhoneProvided = 10,
+        ProfileProvided = 15 ,
+        AddressProvided = 20,
+        Completed = 25
+    }
+}
