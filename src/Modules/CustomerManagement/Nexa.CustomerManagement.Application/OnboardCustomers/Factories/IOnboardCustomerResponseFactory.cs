763be8279d82c20c461fcd3bd9c8005ad7a7f8fc
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 22:09:34 +0200

    feat(customer-management): Add OnboardCustomerDto response factory for onboarding customer flow

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Factories/IOnboardCustomerResponseFactory.cs
@@ -0,0 +1,10 @@
+﻿using Nexa.BuildingBlocks.Application.Factories;
+using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Factories
+{
+    public interface IOnboardCustomerResponseFactory : IResponseFactory<OnboardCustomer, OnboardCustomerDto>
+    {
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Factories/OnboardCustomerResponseFactory.cs
@@ -0,0 +1,52 @@
+﻿using Nexa.BuildingBlocks.Application.Factories;
+using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Factories
+{
+    public class OnboardCustomerResponseFactory : ResponseFactory<OnboardCustomer, OnboardCustomerDto>, IOnboardCustomerResponseFactory
+    {
+        public override Task<OnboardCustomerDto> PrepareDto(OnboardCustomer view)
+        {
+            var dto = new OnboardCustomerDto
+            {
+                Id = view.Id,
+                UserId = view.UserId,
+                Email = view.Email,
+                Status = view.Status
+            };
+
+            PrepareCustomerInfo(view,dto);
+
+            return Task.FromResult(dto);
+        }
+
+        private void PrepareCustomerInfo(OnboardCustomer view , OnboardCustomerDto dto)
+        {
+            if (view.Info == null)
+                return;
+
+            dto.Info = new CustomerInfoDto
+            {
+                FirstName = view.Info.FirstName,
+                LastName = view.Info.LastName,
+                Gender = view.Info.Gender,
+                BirthDate = view.Info.BirthDate,
+            };
+
+
+            if(view.Info.Address != null)
+            {
+                dto.Info.Address = new AddressDto
+                {
+                    Country = view.Info.Address.Country,
+                    State = view.Info.Address.State,
+                    City = view.Info.Address.City,
+                    StreetLine = view.Info.Address.StreetLine,
+                    PostalCode = view.Info.Address.PostalCode,
+                    ZipCode = view.Info.Address.ZipCode
+                };
+            }
+        }
+    }
+}
