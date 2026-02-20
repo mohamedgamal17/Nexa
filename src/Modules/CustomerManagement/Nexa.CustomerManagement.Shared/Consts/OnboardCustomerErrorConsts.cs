16fdefef6d8300733acd6b03b41a09c966f29fcc
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 22:07:22 +0200

    feat(customer-management): Add conflict error message for duplicate OnboardCustomer in onboarding flow

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Shared/Consts/OnboardCustomerErrorConsts.cs
@@ -0,0 +1,10 @@
+﻿using Nexa.BuildingBlocks.Domain.Exceptions;
+
+namespace Nexa.CustomerManagement.Shared.Consts
+{
+    public class OnboardCustomerErrorConsts
+    {
+        public static NexaError OnboardCustomerCreateConflict =
+            new NexaError(nameof(OnboardCustomerCreateConflict), "A customer already exists for the provided UserId. Onboarding cannot be started again.");
+    }
+}
