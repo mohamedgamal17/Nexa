6ed2f60c87a5d6a8284fdc6a0d39a25a98be4e26
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 01:41:54 +0200

    feat(customer-management): Add required update functionality and OnboardCustomerCompletedEvent for onboarding customer

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/OnboardCustomers/Events/OnboardCustomerCompletedEvent.cs
@@ -0,0 +1,22 @@
+﻿using Nexa.BuildingBlocks.Domain.Events;
+using Nexa.CustomerManagement.Domain.Customers;
+
+namespace Nexa.CustomerManagement.Domain.OnboardCustomers.Events
+{
+    public class OnboardCustomerCompletedEvent : IEvent
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public CustomerInfo Info { get; set; }
+        public OnboardCustomerCompletedEvent(string id, string userId, string emailAddress, string phoneNumber, CustomerInfo info)
+        {
+            Id = id;
+            UserId = userId;
+            EmailAddress = emailAddress;
+            PhoneNumber = phoneNumber;
+            Info = info;
+        }
+    }
+}
--- src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/OnboardCustomers/OnboardCustomer.cs
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Domain/OnboardCustomers/OnboardCustomer.cs
@@ -1,17 +1,29 @@
-﻿using Nexa.BuildingBlocks.Domain;
+﻿using MediatR;
+using Nexa.BuildingBlocks.Domain;
+using Nexa.BuildingBlocks.Domain.Results;
 using Nexa.CustomerManagement.Domain.Customers;
+using Nexa.CustomerManagement.Domain.OnboardCustomers.Events;
 using Nexa.CustomerManagement.Shared.Enums;
 
 namespace Nexa.CustomerManagement.Domain.OnboardCustomers
 {
     public class OnboardCustomer : AggregateRoot
     {
-        public string UserId { get; set; }
-        public string? EmailAddress { get; set; }
-        public string? PhoneNumber { get; set; }
-        public CustomerInfo? Info { get; set; }
-        public OnboardCustomerStatus Status { get; set; }
+        public string UserId { get;private set; }
+        public string? EmailAddress { get; private set; }
+        public string? PhoneNumber { get; private set; }
+        public CustomerInfo? Info { get; private set; }
+        public OnboardCustomerStatus Status { get; private set; }
+        public bool IsCompleted => Status == OnboardCustomerStatus.Completed;
+        public bool EmailAddressProvided => EmailAddress != null;
+        public bool PhoneNumberProvided => PhoneNumber != null;
+        public bool CustomerInfoProvided => Info != null;
+        public bool AddressProvided => CustomerInfoProvided && Info?.Address != null;
 
+        public bool HasFullData => EmailAddressProvided
+                && PhoneNumberProvided
+                && CustomerInfoProvided
+                && AddressProvided;
         private OnboardCustomer()
         {
 
@@ -19,6 +31,46 @@ namespace Nexa.CustomerManagement.Domain.OnboardCustomers
         public OnboardCustomer(string userId)
         {
             UserId = userId;
+        }
+
+        public void UpdateEmailAddress(string emailAddress)
+        {
+            ThrowIfCompleted();
+
+            EmailAddress = emailAddress;
+
+        }
+
+        public void UpdatePhoneNumber(string phoneNumber)
+        {
+            ThrowIfCompleted();
+
+            PhoneNumber = phoneNumber;
+        }
+
+        public void UpdateCustomerInfo(CustomerInfo info)
+        {
+            ThrowIfCompleted();
+
+            Info = info;
+
+        }
+        public void MarkAsCompleted()
+        {
+            if(Status == OnboardCustomerStatus.Pending && HasFullData)
+            {
+                var @event = new OnboardCustomerCompletedEvent(Id, UserId, EmailAddress!, PhoneNumber!, Info!);
+
+                AppendEvent(@event);
+            }
+        }
+
+        private void ThrowIfCompleted()
+        {
+            if (IsCompleted)
+            {
+                throw new InvalidOperationException($"{nameof(OnboardCustomer)} current state is completed it should be in pending state to be able to modifiy it's data");
+            }
         }
     }
 }
