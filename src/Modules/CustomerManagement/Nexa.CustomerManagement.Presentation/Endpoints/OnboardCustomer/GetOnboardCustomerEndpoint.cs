573b0111ece9e068e636a197941635c2f59533a5
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 23:26:25 +0200

    feat(customer-management): Add GetOnboardCustomerEndpoint to provide api endpoint for getting current user onboard customer

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/GetOnboardCustomerEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using FastEndpoints;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Queries.GetOnboardCustomerByUserId;
+using Nexa.CustomerManagement.Shared.Dtos;
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class GetOnboardCustomerEndpoint : Endpoint<EmptyRequest, OnboardCustomerDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISecurityContext _securityContext;
+
+        public GetOnboardCustomerEndpoint(IMediator mediator, ISecurityContext securityContext)
+        {
+            _mediator = mediator;
+            _securityContext = securityContext;
+        }
+
+        public override void Configure()
+        {
+            Get("");
+            Group<OnboardCustomerRoutingGroup>();
+        }
+        public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+        {
+            string userId = _securityContext.User!.Id;
+
+            var query = new GetOnboardCustomerByUserIdQuery
+            {
+                UserId = userId
+            };
+
+            var result = await _mediator.Send(query);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+}
