33e776fe96f590081b5794d0b234d95821b2f376
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-20 22:11:38 +0200

    feat(customer-management): Add CreateOnboardCustomerCommandHandler to handle onboarding customer creation

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Commands/CreateOnboardCustomer/CreateOnboardCustomerCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.BuildingBlocks.Domain.Exceptions;
+using Nexa.BuildingBlocks.Domain.Results;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Factories;
+using Nexa.CustomerManagement.Domain;
+using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Shared.Consts;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Commands.CreateOnboardCustomer
+{
+    public class CreateOnboardCustomerCommandHandler : IApplicationRequestHandler<CreateOnboardCustomerCommand, OnboardCustomerDto>
+    {
+        private readonly ICustomerManagementRepository<OnboardCustomer> _onboardCustomerRepository;
+
+        private readonly OnboardCustomerResponseFactory _onboardCustomerResponseFactory;
+        public CreateOnboardCustomerCommandHandler(ICustomerManagementRepository<OnboardCustomer> onboardCustomerRepository, OnboardCustomerResponseFactory onboardCustomerResponseFactory)
+        {
+            _onboardCustomerRepository = onboardCustomerRepository;
+            _onboardCustomerResponseFactory = onboardCustomerResponseFactory;
+        }
+
+        public async Task<Result<OnboardCustomerDto>> Handle(CreateOnboardCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var isonboardCustomerExist = await _onboardCustomerRepository.AnyAsync(x => x.UserId == request.UserId);
+
+            if (isonboardCustomerExist)
+            {
+                return new Result<OnboardCustomerDto>(new ConflictException(OnboardCustomerErrorConsts.OnboardCustomerCreateConflict));
+            }
+
+            var onboardCustomer = new OnboardCustomer(request.UserId);
+
+            await _onboardCustomerRepository.InsertAsync(onboardCustomer);
+
+            return await _onboardCustomerResponseFactory.PrepareDto(onboardCustomer);
+        }
+    }
+}
