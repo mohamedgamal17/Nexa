4e94ae30faa4a1d4b3852706470d172f2af90869
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 21:00:59 +0200

    feat(customer-management): Add CompleteOnboardCustomerEndpoint to provide user endpoint to complete customer onboarding flow

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/CompleteOnboardCustomerEndpoint.cs
@@ -0,0 +1,44 @@
+﻿using FastEndpoints;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.CompleteOnboardCustomer;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class CompleteOnboardCustomerEndpoint : Endpoint<EmptyRequest, CustomerDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISecurityContext _securityContext;
+
+        public CompleteOnboardCustomerEndpoint(IMediator mediator, ISecurityContext securityContext)
+        {
+            _mediator = mediator;
+            _securityContext = securityContext;
+        }
+
+        public override void Configure()
+        {
+            Post("complete");
+
+            Group<OnboardCustomerRoutingGroup>();
+        }
+        public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+        {
+            string userId = _securityContext.User!.Id;
+
+            var command = new CompleteOnboardCustomerCommand
+            {
+                UserId = userId
+            };
+
+            var result = await _mediator.Send(command);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+
+        }
+    }
+}
