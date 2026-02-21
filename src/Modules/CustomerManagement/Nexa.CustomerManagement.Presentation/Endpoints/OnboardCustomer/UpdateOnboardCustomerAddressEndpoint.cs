fbb7948a3853b17dd62c6521c5ad52e4f1b0e179
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 18:20:27 +0200

    feat(customer-management): Add UpdateOnboardCustomerAddressEndpoint for providing user endpoint to update onboard customer address

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/UpdateOnboardCustomerAddressEndpoint.cs
@@ -0,0 +1,56 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerAddress;
+using Nexa.CustomerManagement.Presentation.Requests.Customers;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class UpdateOnboardCustomerAddressEndpoint : Endpoint<AddressRequest, OnboardCustomerDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISecurityContext _securityContext;
+
+        public UpdateOnboardCustomerAddressEndpoint(IMediator mediator, ISecurityContext securityContext)
+        {
+            _mediator = mediator;
+            _securityContext = securityContext;
+        }
+
+        public override void Configure()
+        {
+            Post("address");
+            Group<OnboardCustomerRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(AddressRequest req, CancellationToken ct)
+        {
+            string userId = _securityContext.User!.Id;
+
+            var validator = Resolve<IValidator<AddressRequest>>();
+
+            var validationResult = await validator.ValidateAsync(req);
+
+            if (!validationResult.IsValid)
+            {
+                await SendResultAsync(validationResult.ValidationFailure());
+                return;
+            }
+
+            var command = new UpdateOnboardCustomerAddressCommand
+            {
+                UserId = userId,
+                Address = req.ToAddressModel()
+            };
+
+            var result = await _mediator.Send(command);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Requests/Customers/AddressRequest.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation;
+using Nexa.BuildingBlocks.Domain.Consts;
+using Nexa.CustomerManagement.Application.Customers.Models;
+using Nexa.CustomerManagement.Application.Extensions;
+using Nexa.CustomerManagement.Domain.Customers;
+using Nexa.CustomerManagement.Shared.Consts;
+
+namespace Nexa.CustomerManagement.Presentation.Requests.Customers
+{
+    public class AddressRequest
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string StreetLine { get; set; }
+        public string PostalCode { get; set; }
+        public string ZipCode { get; set; }
+
+
+        public AddressModel ToAddressModel()
+        {
+            return new AddressModel()
+            {
+                Country = Country,
+                City = City,
+                State = State,
+                StreetLine = StreetLine,
+                PostalCode = PostalCode,
+                ZipCode = ZipCode
+            };
+        }
+        public class AddressRequestValidator : AbstractValidator<AddressRequest>
+        {
+            public AddressRequestValidator()
+            {
+                RuleFor(x => x.Country)
+              .IsValidCountryCode(CustomerModuleConsts.SupportedRegions);
+
+                RuleFor(x => x.City)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MaximumLength(AddressTableConstants.CityLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MaxLength.Message, x));
+
+                RuleFor(x => x.State)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MaximumLength(AddressTableConstants.StateLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MaxLength.Message, x));
+
+                RuleFor(x => x.StreetLine)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MaximumLength(AddressTableConstants.StreetLineLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MaxLength.Message, x));
+
+                RuleFor(x => x.PostalCode)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MinimumLength(5)
+                    .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+                    .MaximumLength(AddressTableConstants.PostalCodeLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MaxLength.Message, x));
+
+
+                RuleFor(x => x.ZipCode)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MinimumLength(5)
+                    .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+                    .MaximumLength(AddressTableConstants.ZipCodeLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MaxLength.Message, x));
+            }
+        }
+    }
+}
