a5d071e0859b7bd9bc67e8bff02cca381ca99a3a
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 02:54:10 +0200

    feat(customer-management): Add UpdateOnboardCustomerEmailEndpoint to provide user with endpoint to update onboard customer email

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/UpdateOnboardCustomerEmailEndpoint.cs
@@ -0,0 +1,76 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Domain.Consts;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerEmail;
+using Nexa.CustomerManagement.Domain.OnboardCustomers;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class UpdateOnboardCustomerEmailRequest 
+    {
+        public string Email { get; set; }
+
+
+        public class UpdateOnboardCustomerEmailRequestValidator : AbstractValidator<UpdateOnboardCustomerEmailRequest>
+        {
+            public UpdateOnboardCustomerEmailRequestValidator()
+            {
+                RuleFor(x => x.Email)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MaximumLength(OnboardCustomerTableConsts.EmailAddressLength)
+                    .EmailAddress()
+                    .WithErrorCode(GlobalErrorConsts.InvalidEmailAddress.Code)
+                    .WithMessage(GlobalErrorConsts.InvalidEmailAddress.Message);
+            }
+        }
+       
+    }
+    public class UpdateOnboardCustomerEmailEndpoint : Endpoint<UpdateOnboardCustomerEmailRequest, OnboardCustomerDto>
+    {
+        private readonly ISecurityContext _securityContext;
+        private readonly IMediator _mediator;
+        public UpdateOnboardCustomerEmailEndpoint(ISecurityContext securityContext, IMediator mediator)
+        {
+            _securityContext = securityContext;
+            _mediator = mediator;
+        }
+
+        public override void Configure()
+        {
+            Post("email");
+            Group<OnboardCustomerRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(UpdateOnboardCustomerEmailRequest req, CancellationToken ct)
+        {
+            string userId = _securityContext.User!.Id;
+
+            var validator = Resolve<IValidator<UpdateOnboardCustomerEmailRequest>>();
+
+            var validationResult = await validator.ValidateAsync(req);
+
+            if (!validationResult.IsValid)
+            {
+                await SendResultAsync(validationResult.ValidationFailure());
+            }
+
+            var command = new UpdateOnboardCustomerEmailCommand
+            {
+                UserId = userId,
+                EmailAddress = req.Email
+            };
+
+            var result = await _mediator.Send(command);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+}
