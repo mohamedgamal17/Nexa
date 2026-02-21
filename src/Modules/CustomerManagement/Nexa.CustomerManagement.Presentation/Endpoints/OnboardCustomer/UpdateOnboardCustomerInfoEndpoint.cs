3be2baa70904aa6b389232f648332faf0c089c3f
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 06:18:32 +0200

    feat(customer-management): Add UpdateOnboardCustomerInfoEndpoint to provide user api endpoint to update onboard customer info during onboard customer flow

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Endpoints/OnboardCustomer/UpdateOnboardCustomerInfoEndpoint.cs
@@ -0,0 +1,61 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using MediatR;
+using Nexa.BuildingBlocks.Application.Abstractions.Security;
+using Nexa.BuildingBlocks.Infrastructure.Extensions;
+using Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerInfo;
+using Nexa.CustomerManagement.Presentation.Requests.Customers;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Presentation.Endpoints.OnboardCustomer
+{
+    public class UpdateOnboardCustomerInfoEndpoint : Endpoint<CustomerInfoRequest, OnboardCustomerDto>
+    {
+        private readonly IMediator _mediator;
+        private readonly ISecurityContext _securityContext;
+
+        public UpdateOnboardCustomerInfoEndpoint(IMediator mediator, ISecurityContext securityContext)
+        {
+            _mediator = mediator;
+            _securityContext = securityContext;
+        }
+
+        public override void Configure()
+        {
+            Post("info");
+
+            Group<OnboardCustomerRoutingGroup>();
+        }
+
+        public override async Task HandleAsync(CustomerInfoRequest req, CancellationToken ct)
+        {
+            string userId = _securityContext.User!.Id;
+
+            var validator = Resolve<IValidator<CustomerInfoRequest>>();
+
+            var validationResult = await validator.ValidateAsync(req);
+
+            if (!validationResult.IsValid)
+            {
+                await SendResultAsync(validationResult.ValidationFailure());
+                return;
+            }
+
+            var command = new UpdateOnboardCustomerInfoCommand
+            {
+                UserId = userId,
+                FirstName = req.FirstName,
+                LastName = req.LastName,
+                Gender = req.Gender,
+                BirthDate = req.BirthDate
+            };
+
+            var result = await _mediator.Send(command);
+
+            var response = result.ToOk();
+
+            await SendResultAsync(response);
+        }
+    }
+    
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Presentation/Requests/Customers/CustomerInfoRequest.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Nexa.BuildingBlocks.Domain.Consts;
+using Nexa.CustomerManagement.Domain.Customers;
+using Nexa.CustomerManagement.Shared.Enums;
+
+namespace Nexa.CustomerManagement.Presentation.Requests.Customers
+{
+    public class CustomerInfoRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+        public class CustomerInfoRequestValidator : AbstractValidator<CustomerInfoRequest>
+        {
+            public CustomerInfoRequestValidator()
+            {
+                RuleFor(x => x.FirstName)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MinimumLength(2)
+                    .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+                    .MaximumLength(CustomerInfoTableConsts.FirstNameLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x));
+
+
+                RuleFor(x => x.LastName)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .MinimumLength(2)
+                    .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+                    .MaximumLength(CustomerInfoTableConsts.LastNameLength)
+                    .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                    .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x));
+
+                RuleFor(x => x.BirthDate)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message)
+                    .Must(x => x <= DateTime.UtcNow.AddYears(-18))
+                    .WithErrorCode(GlobalErrorConsts.InvalidBirthDate.Code)
+                    .WithMessage(GlobalErrorConsts.InvalidBirthDate.Message);
+
+                RuleFor(x => x.Gender)
+                    .NotEmpty()
+                    .WithErrorCode(GlobalErrorConsts.Required.Code)
+                    .WithMessage(GlobalErrorConsts.Required.Message);
+            }
+        }
+    }
+}
