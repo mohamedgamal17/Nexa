ca9fce8bfdf9978e5bf7ec1572482a118d1f6ada
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 03:56:24 +0200

    feat(customer-management): Add base UpdateOnboardCustomerPhoneCommand to provide updating to onboard customer phone number

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Commands/UpdateOnboardCustomerPhone/UpdateOnboardCustomerPhoneCommand.cs
@@ -0,0 +1,12 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerPhone
+{
+    public class UpdateOnboardCustomerPhoneCommand : ICommand<OnboardCustomerDto>
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+}
