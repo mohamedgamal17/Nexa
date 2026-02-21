163a72ee5cc8d9d6b6580a9ac6682bd58d797798
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-02-21 16:50:58 +0200

    feat(customer-management): add command to update onboard customer address

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/OnboardCustomers/Commands/UpdateOnboardCustomerAddress/UpdateOnboardCustomerAddressCommand.cs
@@ -0,0 +1,14 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Application.Customers.Models;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.OnboardCustomers.Commands.UpdateOnboardCustomerAddress
+{
+    public class UpdateOnboardCustomerAddressCommand : ICommand<OnboardCustomerDto>
+    {
+        public string UserId { get; set; }
+        public AddressModel Address { get; set; }
+
+    }
+
+}
