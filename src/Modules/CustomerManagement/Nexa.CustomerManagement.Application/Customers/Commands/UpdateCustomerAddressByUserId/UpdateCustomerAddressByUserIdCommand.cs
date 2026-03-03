bbf0ba8061552e71b437944899312a23f6767b19
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 03:55:53 +0200

    feat(customer-management): Add UpdateCustomerAddressByUserIdCommand to update customer address

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/UpdateCustomerAddressByUserId/UpdateCustomerAddressByUserIdCommand.cs
@@ -0,0 +1,13 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Application.Customers.Models;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.Customers.Commands.UpdateCustomerAddressByUserId
+{
+    public class UpdateCustomerAddressByUserIdCommand : ICommand<CustomerDto>
+    {
+        public string UserId { get; set; }
+        public AddressModel  Addres { get; set; }
+    }
+
+}
