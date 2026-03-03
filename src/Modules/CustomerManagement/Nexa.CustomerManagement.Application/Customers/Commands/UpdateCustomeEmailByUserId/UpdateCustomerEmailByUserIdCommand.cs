b1e4283fc9205131017bda3a62d13689ccc38dd3
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 02:13:57 +0200

    feat(customer-management): Add UpdateCustomerEmailByUserIdCommand

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/UpdateCustomeEmailByUserId/UpdateCustomerEmailByUserIdCommand.cs
@@ -0,0 +1,11 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.Customers.Commands.UpdateCustomeEmailByUserId
+{
+    public class UpdateCustomerEmailByUserIdCommand : ICommand<CustomerDto>
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+    }
+}
