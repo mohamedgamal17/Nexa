be80a26c18fe66da126bebf7c8ce14c1fac0e705
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 02:39:52 +0200

    feat(customer-management): Add UpdateCustomerPhoneByUserIdCommand to update customer phone by UserId

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/UpdateCustomerPhoneByUserId/UpdateCustomerPhoneByUserIdCommand.cs
@@ -0,0 +1,12 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Shared.Dtos;
+namespace Nexa.CustomerManagement.Application.Customers.Commands.UpdateCustomerPhoneByUserId
+{
+    public class UpdateCustomerPhoneByUserIdCommand : ICommand<CustomerDto>
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+
+}
