cdc31548b785c2fa34c37e4907a65de2c1f5a6dc
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-03-03 03:32:34 +0200

    feat(customer-management): Add UpdateCustomerInfoByUserIdCommand to update customer info

---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Commands/UpdateCustomerInfoByUserId/UpdateCustomerInfoByUserIdCommand.cs
@@ -0,0 +1,12 @@
+﻿using Nexa.BuildingBlocks.Application.Requests;
+using Nexa.CustomerManagement.Application.Customers.Models;
+using Nexa.CustomerManagement.Shared.Dtos;
+
+namespace Nexa.CustomerManagement.Application.Customers.Commands.UpdateCustomerInfoByUserId
+{
+    public class UpdateCustomerInfoByUserIdCommand : ICommand<CustomerDto>
+    {
+        public string UserId { get; set; }
+        public CustomerInfoModel Info { get; set; }
+    }
+}
---
+++ src/Modules/CustomerManagement/Nexa.CustomerManagement.Application/Customers/Models/CustomerInfoModel.cs
@@ -0,0 +1,58 @@
+﻿
+using FluentValidation;
+using Nexa.BuildingBlocks.Domain.Consts;
+using Nexa.CustomerManagement.Domain.Customers;
+using Nexa.CustomerManagement.Shared.Enums;
+
+namespace Nexa.CustomerManagement.Application.Customers.Models
+{
+    public class CustomerInfoModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public class CustomerInfoModelValidator : AbstractValidator<CustomerInfoModel>
+    {
+        public CustomerInfoModelValidator()
+        {
+            RuleFor(x => x.FirstName)
+               .NotEmpty()
+               .WithErrorCode(GlobalErrorConsts.Required.Code)
+               .WithMessage(GlobalErrorConsts.Required.Message)
+               .MinimumLength(2)
+               .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+               .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+               .MaximumLength(CustomerInfoTableConsts.FirstNameLength)
+               .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+               .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x));
+
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithErrorCode(GlobalErrorConsts.Required.Code)
+                .WithMessage(GlobalErrorConsts.Required.Message)
+                .MinimumLength(2)
+                .WithErrorCode(GlobalErrorConsts.MinLength.Code)
+                .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x))
+                .MaximumLength(CustomerInfoTableConsts.LastNameLength)
+                .WithErrorCode(GlobalErrorConsts.MaxLength.Code)
+                .WithMessage(x => string.Format(GlobalErrorConsts.MinLength.Message, x));
+
+            RuleFor(x => x.BirthDate)
+                .NotEmpty()
+                .WithErrorCode(GlobalErrorConsts.Required.Code)
+                .WithMessage(GlobalErrorConsts.Required.Message)
+                .Must(x => x <= DateTime.UtcNow.AddYears(-18))
+                .WithErrorCode(GlobalErrorConsts.InvalidBirthDate.Code)
+                .WithMessage(GlobalErrorConsts.InvalidBirthDate.Message);
+
+            RuleFor(x => x.Gender)
+                .NotEmpty()
+                .WithErrorCode(GlobalErrorConsts.Required.Code)
+                .WithMessage(GlobalErrorConsts.Required.Message);
+        }
+    }
+}
